1a8e99802641e964eaae3dea26a323a7e0a0e47a
jhalliday <jhalliday@c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70>
2006-10-30 10:55:08 +0000

    Upgraded service wrapper (http://wrapper.tanukisoftware.org/) from 3.0.5 to 3.2.3:    Modified TransactionServerService.java and RecoveryManagerService.java to use the new API.    Swapped in new wrapper.jar, .so, .dll, .exe files, deleted (obsolete?) realpath.    Modified .conf files to use relative paths to minimise install time hassles.
    git-svn-id: https://anonsvn.jboss.org/repos/labs/labs/jbosstm/trunk@7224 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- ArjunaCore/arjuna/services/classes/com/arjuna/ats/arjuna/services/recovery/RecoveryManagerService.java
+++ ArjunaCore/arjuna/services/classes/com/arjuna/ats/arjuna/services/recovery/RecoveryManagerService.java
@@ -1,20 +1,20 @@
 /*
  * JBoss, Home of Professional Open Source
- * Copyright 2006, JBoss Inc., and others contributors as indicated 
- * by the @authors tag. All rights reserved. 
+ * Copyright 2006, JBoss Inc., and others contributors as indicated
+ * by the @authors tag. All rights reserved.
  * See the copyright.txt in the distribution for a
- * full listing of individual contributors. 
+ * full listing of individual contributors.
  * This copyrighted material is made available to anyone wishing to use,
  * modify, copy, or redistribute it subject to the terms and conditions
  * of the GNU Lesser General Public License, v. 2.1.
- * This program is distributed in the hope that it will be useful, but WITHOUT A 
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
  * You should have received a copy of the GNU Lesser General Public License,
  * v.2.1 along with this distribution; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA  02110-1301, USA.
- * 
+ *
  * (C) 2005-2006,
  * @author JBoss Inc.
  */
@@ -23,7 +23,8 @@ package com.arjuna.ats.arjuna.services.recovery;
 import com.arjuna.ats.internal.arjuna.recovery.*;
 import com.arjuna.ats.arjuna.common.Uid;
 
-import com.silveregg.wrapper.*;
+import org.tanukisoftware.wrapper.WrapperListener;
+import org.tanukisoftware.wrapper.WrapperManager;
 
 /*
  * Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004
@@ -36,7 +37,7 @@ import com.silveregg.wrapper.*;
  * $Id: RecoveryManagerService.java 2342 2006-03-30 13:06:17Z  $
  */
 
-public class RecoveryManagerService implements com.silveregg.wrapper.WrapperListener
+public class RecoveryManagerService implements WrapperListener
 {
     private final static int FAILED_TO_START_RETURN_CODE = 1;
 
