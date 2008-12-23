9e7d5c997e2a7d56ea6cba540569444794a21a60
mark.little@jboss.com <mark.little@jboss.com@c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70>
2008-12-23 18:51:02 +0000

    fixed typo
    git-svn-id: https://anonsvn.jboss.org/repos/labs/labs/jbosstm/trunk@24483 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- ArjunaCore/arjuna/classes/com/arjuna/ats/internal/arjuna/recovery/ExpiredTransactionScanner.java
+++ ArjunaCore/arjuna/classes/com/arjuna/ats/internal/arjuna/recovery/ExpiredTransactionScanner.java
@@ -58,7 +58,7 @@ import com.arjuna.common.util.logging.*;
 
 /**
  * This class is a plug-in module for the recovery manager. This class is
- * responsible for the removing ransaction status manager items that are too
+ * responsible for the removing transaction status manager items that are too
  * old.
  */
 
