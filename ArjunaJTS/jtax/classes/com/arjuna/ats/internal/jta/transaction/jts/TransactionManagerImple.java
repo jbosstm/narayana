48dcf2c3a41d0d2334dbfa518965222254231b47
jhalliday <jhalliday@c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70>
2009-01-19 13:02:30 +0000

    Fixed JTS handling of TransactionManager.resume(null).  JBTM-466
    git-svn-id: https://anonsvn.jboss.org/repos/labs/labs/jbosstm/trunk@24782 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- ArjunaJTS/jtax/classes/com/arjuna/ats/internal/jta/transaction/jts/TransactionManagerImple.java
+++ ArjunaJTS/jtax/classes/com/arjuna/ats/internal/jta/transaction/jts/TransactionManagerImple.java
@@ -1,8 +1,8 @@
 /*
  * JBoss, Home of Professional Open Source
  * Copyright 2006, Red Hat Middleware LLC, and individual contributors
- * as indicated by the @author tags. 
- * See the copyright.txt in the distribution for a full listing 
+ * as indicated by the @author tags.
+ * See the copyright.txt in the distribution for a full listing
  * of individual contributors.
  * This copyrighted material is made available to anyone wishing to use,
  * modify, copy, or redistribute it subject to the terms and conditions
@@ -14,7 +14,7 @@
  * v.2.1 along with this distribution; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA  02110-1301, USA.
- * 
+ *
  * (C) 2005-2006,
  * @author JBoss Inc.
  */
@@ -52,7 +52,7 @@ import java.util.Hashtable;
 
 /**
  * An implementation of javax.transaction.TransactionManager.
- * 
+ *
  * @author Mark Little (mark_little@hp.com)
  * @version $Id: TransactionManagerImple.java 2342 2006-03-30 13:06:17Z  $
  * @since JTS 1.2.4.
@@ -122,35 +122,44 @@ public class TransactionManagerImple extends BaseTransaction implements
 
 		super.checkTransactionState();
 
-		if (which instanceof TransactionImple)
-		{
-			TransactionImple theTransaction = (TransactionImple) which;
+        if (which != null)
+        {
+            if (which instanceof TransactionImple)
+            {
+                TransactionImple theTransaction = (TransactionImple) which;
 
-			try
-			{
-				ControlWrapper cont = theTransaction.getControlWrapper();
+                try
+                {
+                    ControlWrapper cont = theTransaction.getControlWrapper();
 
-				OTSImpleManager.current().resumeWrapper(cont);
+                    OTSImpleManager.current().resumeWrapper(cont);
 
-				cont = null;
-				theTransaction = null;
-			}
-			catch (org.omg.CosTransactions.InvalidControl e1)
-			{
-				//throw new InvalidTransactionException();
-			}
-			catch (org.omg.CORBA.SystemException e2)
-			{
-				throw new javax.transaction.SystemException();
-			}
-		}
-		else
-			throw new InvalidTransactionException();
-	}
+                    cont = null;
+                    theTransaction = null;
+                }
+                catch (org.omg.CosTransactions.InvalidControl e1)
+                {
+                    //throw new InvalidTransactionException();
+                }
+                catch (org.omg.CORBA.SystemException e2)
+                {
+                    throw new javax.transaction.SystemException();
+                }
+            }
+            else
+            {
+                throw new InvalidTransactionException();
+            }
+        }
+        else
+        {
+            // resume a null tx == nothing to do
+        }
+    }
 
 	/**
 	 * Creates a TransactionManageImple from the given information.
-	 * 
+	 *
 	 * @param obj
 	 * @param name
 	 * @param nameCtx
