f43976407eeb073d72748c1ba3150ece0f747a79
mmusgrov <mmusgrov@c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70>
2009-03-12 16:44:08 +0000

    Add a different test for descriminating between JTA and JTS
    JBTM-512
    
    
    git-svn-id: https://anonsvn.jboss.org/repos/labs/labs/jbosstm/trunk@25622 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- ArjunaJTA/tsmx/bin/com/arjuna/ats/tools/objectstorebrowser/ToolInitializer.java
+++ ArjunaJTA/tsmx/bin/com/arjuna/ats/tools/objectstorebrowser/ToolInitializer.java
@@ -7,6 +7,7 @@ import com.arjuna.ats.arjuna.common.Uid;
 import com.arjuna.ats.internal.jta.transaction.arjunacore.TransactionImple;
 import com.arjuna.ats.internal.arjuna.utils.XATxConverter;
 import com.arjuna.ats.jta.xa.XidImple;
+import com.arjuna.ats.jta.utils.JNDIManager;
 
 import javax.transaction.Transaction;
 import javax.transaction.xa.Xid;
@@ -14,21 +15,29 @@ import java.util.Map;
 
 public class ToolInitializer implements IToolInitializer
 {
+    static String JTS_TM = "com.arjuna.ats.internal.jta.transaction.jts.TransactionManagerImple";
+
     public void initialize(ToolPlugin plugin)
     {
+        String tmClassName = JNDIManager.getTransactionManagerImplementationClassname();
+
         com.arjuna.ats.internal.jta.Implementations.initialise();   // needed for XAResourceRecord
 
-        try
+        /* test whether we are using the JTS */
+        if (JTS_TM.equals(tmClassName))
         {
-            Class<?> c1 = Class.forName("com.arjuna.ats.internal.jts.Implementations");
-            Class<?> c2 = Class.forName("com.arjuna.ats.internal.jta.Implementationsx"); // needed for XAResourceRecord
+            try
+            {
+                Class<?> c1 = Class.forName("com.arjuna.ats.internal.jts.Implementations");
+                Class<?> c2 = Class.forName("com.arjuna.ats.internal.jta.Implementationsx"); // needed for XAResourceRecord
 
-            c1.getMethod("initialise").invoke(null);
-            c2.getMethod("initialise").invoke(null);
-        }
-        catch (Exception e)
-        {
-            // not JTS
+                c1.getMethod("initialise").invoke(null);
+                c2.getMethod("initialise").invoke(null);
+            }
+            catch (Exception e)
+            {
+                // not JTS
+            }
         }
 
         InFlightTransactionPseudoStore.setTransactionLister(new TransactionLister(){
