9427e395514074531fd3132aba7f662be2e741eb
mark.little@jboss.com <mark.little@jboss.com@c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70>
2010-06-22 23:33:06 +0000

    https://jira.jboss.org/browse/JBTM-754
    git-svn-id: https://anonsvn.jboss.org/repos/labs/labs/jbosstm/trunk@33600 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- ArjunaJTA/jta/classes/com/arjuna/ats/jta/exceptions/InactiveTransactionException.java
+++ ArjunaJTA/jta/classes/com/arjuna/ats/jta/exceptions/InactiveTransactionException.java
@@ -31,8 +31,6 @@
 
 package com.arjuna.ats.jta.exceptions;
 
-import javax.transaction.SystemException;
-
 /**
  * Exception may be thrown under certain circumstances. Typically
  * this is when we would like to throw IllegalStateException to indicate
@@ -45,7 +43,7 @@ import javax.transaction.SystemException;
  * @since JTS 1.2.4.
  */
 
-public class InactiveTransactionException extends SystemException
+public class InactiveTransactionException extends IllegalStateException
 {
     static final long serialVersionUID = -8288622240488128416L;
     
@@ -58,6 +56,5 @@ public class InactiveTransactionException extends SystemException
     {
 	super(s);
     }
-
 }
 
