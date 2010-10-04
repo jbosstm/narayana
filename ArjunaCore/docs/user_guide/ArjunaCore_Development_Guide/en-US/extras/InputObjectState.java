46fe89dae69398221a652efafd049051e400d81e
misty@redhat.com <misty@redhat.com@c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70>
2010-10-04 06:56:38 +0000

    Removed ArjunaCore Development Guide from old location.
    git-svn-id: https://anonsvn.jboss.org/repos/labs/labs/jbosstm/trunk@35399 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- ArjunaCore/docs/user_guide/ArjunaCore_Development_Guide/en-US/extras/InputObjectState.java
+++
@@ -1,9 +0,0 @@
-class InputObjectState extends InputBuffer
-{
-    public OutputObjectState (Uid newUid, String typeName, byte[] b);
-
-    public boolean notempty ();
-    public int size ();
-    public Uid stateUid ();
-    public String type ();
-};
