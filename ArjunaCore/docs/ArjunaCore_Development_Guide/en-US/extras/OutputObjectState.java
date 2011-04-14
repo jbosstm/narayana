aa80eeac3b414aca4f015a584ab993be2ba09ff6
tomjenkinson <tomjenkinson@c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70>
2011-04-14 09:26:18 +0000

    JBTM-820 moved to development_guide
    git-svn-id: https://anonsvn.jboss.org/repos/labs/labs/jbosstm/trunk@36927 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- ArjunaCore/docs/ArjunaCore_Development_Guide/en-US/extras/OutputObjectState.java
+++
@@ -1,9 +0,0 @@
-class OutputObjectState extends OutputBuffer
-{
-    public OutputObjectState (Uid newUid, String typeName);
-
-    public boolean notempty ();
-    public int size ();
-    public Uid stateUid ();
-    public String type ();
-};
