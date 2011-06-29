7b1d4413cdf639a5b91d0cff50dbb8169dfbbbe1
tomjenkinson <tomjenkinson@c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70>
2011-06-29 13:39:42 +0000

    updated to use an annotation to define the servlet
    git-svn-id: https://anonsvn.jboss.org/repos/labs/labs/jbosstm/trunk@37166 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- ArjunaJTA/examples/integration/src/main/java/org/jboss/narayana/examples/servlet/SimpleServlet.java
+++ ArjunaJTA/examples/integration/src/main/java/org/jboss/narayana/examples/servlet/SimpleServlet.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 import javax.ejb.EJB;
 import javax.naming.InitialContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +14,7 @@ import javax.transaction.UserTransaction;
 
 import org.jboss.narayana.examples.ejb.SimpleEJB;
 
+@WebServlet(displayName = "hello", urlPatterns = "/hello")
 public class SimpleServlet extends HttpServlet {
 
 	@EJB
