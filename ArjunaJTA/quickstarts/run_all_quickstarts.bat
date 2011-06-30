88b339842bfd67f3d8dbf6f503746c6f181878e0
tomjenkinson <tomjenkinson@c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70>
2011-06-30 11:29:19 +0000

    JBTM-854 add the bat file to run all quickstarts
    git-svn-id: https://anonsvn.jboss.org/repos/labs/labs/jbosstm/trunk@37178 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ ArjunaJTA/quickstarts/run_all_quickstarts.bat
@@ -0,0 +1,17 @@
+@echo off
+
+echo "Running all quickstarts"
+
+set OLDPWD=%cd%
+
+for /R * %%i in (.) do (
+	if exist %%i\run.sh (
+ 		cd %%i
+ 		call run.bat %*
+		IF %ERRORLEVEL% NEQ 0 exit -1
+	)
+)
+
+cd /d %OLDPWD%
+
+echo "All quickstarts ran OK"
