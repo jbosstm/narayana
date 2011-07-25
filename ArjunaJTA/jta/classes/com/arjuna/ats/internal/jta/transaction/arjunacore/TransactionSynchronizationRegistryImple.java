246a520340e9e79f6edb82bc2398b1723b159095
jhalliday <jhalliday@c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70>
2011-07-25 16:11:46 +0000

    Improve TransactionSynchronizationRegistry performance. JBTM-853
    git-svn-id: https://anonsvn.jboss.org/repos/labs/labs/jbosstm/trunk@37304 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- ArjunaJTA/jta/classes/com/arjuna/ats/internal/jta/transaction/arjunacore/TransactionSynchronizationRegistryImple.java
+++ ArjunaJTA/jta/classes/com/arjuna/ats/internal/jta/transaction/arjunacore/TransactionSynchronizationRegistryImple.java
@@ -25,11 +25,12 @@ package com.arjuna.ats.internal.jta.transaction.arjunacore;
 import com.arjuna.ats.jta.logging.jtaLogger;
 import com.arjuna.ats.internal.jta.resources.arjunacore.SynchronizationImple;
 
-
 import javax.naming.Context;
 import javax.naming.Name;
 import javax.naming.spi.ObjectFactory;
 import javax.transaction.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.Serializable;
 import java.util.Hashtable;
 
@@ -50,176 +51,180 @@ public class TransactionSynchronizationRegistryImple implements TransactionSynch
 
     private static final long serialVersionUID = 1L;
 
-	public Object getObjectInstance(Object obj, Name name, Context nameCtx, Hashtable environment) throws Exception
-	{
-		return this;
-	}
+    // cached for performance. Note: must set tm config before instantiating a TSRImple instance.
+    private transient javax.transaction.TransactionManager tm = com.arjuna.ats.jta.TransactionManager.transactionManager();
+
+    private void readObject(ObjectInputStream objectInputStream) throws IOException, ClassNotFoundException {
+        objectInputStream.defaultReadObject();
+        tm = com.arjuna.ats.jta.TransactionManager.transactionManager();
+    }
+
+    public Object getObjectInstance(Object obj, Name name, Context nameCtx, Hashtable environment) throws Exception
+    {
+        return this;
+    }
 
     // Return an opaque object to represent the transaction bound to the current thread at the time this method is called.
-        public Object getTransactionKey()
-        {
-                if (jtaLogger.logger.isTraceEnabled()) {
-                    jtaLogger.logger.trace("TransactionSynchronizationRegistryImple.getTransactionKey");
-                }
-
-                javax.transaction.TransactionManager tm = com.arjuna.ats.jta.TransactionManager.transactionManager();
-                TransactionImple transactionImple = null;
-                try
-                {
-                        transactionImple = (TransactionImple)tm.getTransaction();
-                }
-                catch (SystemException e)
-                {
-                        throw new RuntimeException(jtaLogger.i18NLogger.get_transaction_arjunacore_systemexception(), e);
-                }
-
-                if (transactionImple == null) {
-                        return null;
-                } else {
-                        return transactionImple.get_uid();
-                }
+    public Object getTransactionKey()
+    {
+        if (jtaLogger.logger.isTraceEnabled()) {
+            jtaLogger.logger.trace("TransactionSynchronizationRegistryImple.getTransactionKey");
         }
 
-        // Add or replace an object in the Map of resources being managed for the transaction bound to the current thread at the time this method is called.
-        public void putResource(Object key, Object value)
+        TransactionImple transactionImple = null;
+        try
         {
-                if (jtaLogger.logger.isTraceEnabled()) {
-                    jtaLogger.logger.trace("TransactionSynchronizationRegistryImple.putResource");
-                }
-
-                if(key ==  null)
-                {
-                        throw new NullPointerException();
-                }
-
-                TransactionImple transactionImple = getTransactionImple();
-                transactionImple.putTxLocalResource(key, value);
+            transactionImple = (TransactionImple)tm.getTransaction();
+        }
+        catch (SystemException e)
+        {
+            throw new RuntimeException(jtaLogger.i18NLogger.get_transaction_arjunacore_systemexception(), e);
         }
 
-        // Get an object from the Map of resources being managed for the transaction bound to the current thread at the time this method is called.
-        public Object getResource(Object key)
-        {
-                if (jtaLogger.logger.isTraceEnabled()) {
-                    jtaLogger.logger.trace("TransactionSynchronizationRegistryImple.getResource");
-                }
+        if (transactionImple == null) {
+            return null;
+        } else {
+            return transactionImple.get_uid();
+        }
+    }
 
-                if(key ==  null)
-                {
-                        throw new NullPointerException();
-                }
-
-                TransactionImple transactionImple = getTransactionImple();
-                return transactionImple.getTxLocalResource(key);
+    // Add or replace an object in the Map of resources being managed for the transaction bound to the current thread at the time this method is called.
+    public void putResource(Object key, Object value)
+    {
+        if (jtaLogger.logger.isTraceEnabled()) {
+            jtaLogger.logger.trace("TransactionSynchronizationRegistryImple.putResource");
         }
 
-        // Register a Synchronization instance with special ordering semantics.
-        public void registerInterposedSynchronization(Synchronization synchronization)
+        if(key ==  null)
         {
-                if (jtaLogger.logger.isTraceEnabled()) {
-                    jtaLogger.logger.trace("TransactionSynchronizationRegistryImple.registerInterposedSynchronization");
-                }
-
-                TransactionImple transactionImple = getTransactionImple();
-
-                try
-                {
-                        transactionImple.registerSynchronizationImple(new SynchronizationImple(synchronization, true));
-                }
-                catch (RollbackException e)
-                {
-                        throw new com.arjuna.ats.jta.exceptions.RollbackException(jtaLogger.i18NLogger.get_transaction_arjunacore_syncrollbackexception(), e);
-                }
-                catch (SystemException e)
-                {
-                        throw new RuntimeException(jtaLogger.i18NLogger.get_transaction_arjunacore_systemexception(), e);
-                }
+            throw new NullPointerException();
         }
 
-        // Return the status of the transaction bound to the current thread at the time this method is called.
-        public int getTransactionStatus()
-        {
-                if (jtaLogger.logger.isTraceEnabled()) {
-                    jtaLogger.logger.trace("TransactionSynchronizationRegistryImple.getTransactionStatus");
-                }
+        TransactionImple transactionImple = getTransactionImple();
+        transactionImple.putTxLocalResource(key, value);
+    }
 
-                javax.transaction.TransactionManager tm = com.arjuna.ats.jta.TransactionManager.transactionManager();
-                try
-                {
-                        return tm.getStatus();
-                }
-                catch(SystemException e)
-                {
-                        throw new RuntimeException(jtaLogger.i18NLogger.get_transaction_arjunacore_systemexception(), e);
-                }
-
+    // Get an object from the Map of resources being managed for the transaction bound to the current thread at the time this method is called.
+    public Object getResource(Object key)
+    {
+        if (jtaLogger.logger.isTraceEnabled()) {
+            jtaLogger.logger.trace("TransactionSynchronizationRegistryImple.getResource");
         }
 
-        // Set the rollbackOnly status of the transaction bound to the current thread at the time this method is called.
-        public void setRollbackOnly()
+        if(key ==  null)
         {
-                if (jtaLogger.logger.isTraceEnabled()) {
-                    jtaLogger.logger.trace("TransactionSynchronizationRegistryImple.setRollbackOnly");
-                }
-
-                javax.transaction.TransactionManager tm = com.arjuna.ats.jta.TransactionManager.transactionManager();
-                try
-                {
-                        Transaction transaction = tm.getTransaction();
-
-                        if(transaction == null)
-                        {
-                                throw new IllegalStateException();
-                        }
-
-                        tm.setRollbackOnly();
-                }
-                catch (SystemException e)
-                {
-                        throw new RuntimeException(jtaLogger.i18NLogger.get_transaction_arjunacore_systemexception(), e);
-                }
+            throw new NullPointerException();
         }
 
-        // Get the rollbackOnly status of the transaction bound to the current thread at the time this method is called.
-        public boolean getRollbackOnly()
-        {
-                if (jtaLogger.logger.isTraceEnabled()) {
-                    jtaLogger.logger.trace("TransactionSynchronizationRegistryImple.getRollbackOnly");
-                }
+        TransactionImple transactionImple = getTransactionImple();
+        return transactionImple.getTxLocalResource(key);
+    }
 
-                TransactionImple transactionImple = getTransactionImple();
-
-                if(transactionImple == null) {
-                        throw new IllegalStateException();
-                }
-
-                try
-                {
-                        return (transactionImple.getStatus() == Status.STATUS_MARKED_ROLLBACK);
-                }
-                catch (SystemException e)
-                {
-                        throw new RuntimeException(jtaLogger.i18NLogger.get_transaction_arjunacore_systemexception(), e);
-                }
+    // Register a Synchronization instance with special ordering semantics.
+    public void registerInterposedSynchronization(Synchronization synchronization)
+    {
+        if (jtaLogger.logger.isTraceEnabled()) {
+            jtaLogger.logger.trace("TransactionSynchronizationRegistryImple.registerInterposedSynchronization");
         }
 
-        private TransactionImple getTransactionImple() throws IllegalStateException
+        TransactionImple transactionImple = getTransactionImple();
+
+        try
         {
-                javax.transaction.TransactionManager tm = com.arjuna.ats.jta.TransactionManager.transactionManager();
-                TransactionImple transactionImple = null;
-                try
-                {
-                        transactionImple = (TransactionImple)tm.getTransaction();
-                }
-                catch (SystemException e)
-                {
-                        throw new RuntimeException(jtaLogger.i18NLogger.get_transaction_arjunacore_systemexception(), e);
-                }
-
-                if(transactionImple == null)
-                {
-                        throw new IllegalStateException();
-                }
-
-                return transactionImple;
+            transactionImple.registerSynchronizationImple(new SynchronizationImple(synchronization, true));
         }
+        catch (RollbackException e)
+        {
+            throw new com.arjuna.ats.jta.exceptions.RollbackException(jtaLogger.i18NLogger.get_transaction_arjunacore_syncrollbackexception(), e);
+        }
+        catch (SystemException e)
+        {
+            throw new RuntimeException(jtaLogger.i18NLogger.get_transaction_arjunacore_systemexception(), e);
+        }
+    }
+
+    // Return the status of the transaction bound to the current thread at the time this method is called.
+    public int getTransactionStatus()
+    {
+        if (jtaLogger.logger.isTraceEnabled()) {
+            jtaLogger.logger.trace("TransactionSynchronizationRegistryImple.getTransactionStatus");
+        }
+
+        try
+        {
+            return tm.getStatus();
+        }
+        catch(SystemException e)
+        {
+            throw new RuntimeException(jtaLogger.i18NLogger.get_transaction_arjunacore_systemexception(), e);
+        }
+
+    }
+
+    // Set the rollbackOnly status of the transaction bound to the current thread at the time this method is called.
+    public void setRollbackOnly()
+    {
+        if (jtaLogger.logger.isTraceEnabled()) {
+            jtaLogger.logger.trace("TransactionSynchronizationRegistryImple.setRollbackOnly");
+        }
+
+        try
+        {
+            Transaction transaction = tm.getTransaction();
+
+            if(transaction == null)
+            {
+                throw new IllegalStateException();
+            }
+
+            tm.setRollbackOnly();
+        }
+        catch (SystemException e)
+        {
+            throw new RuntimeException(jtaLogger.i18NLogger.get_transaction_arjunacore_systemexception(), e);
+        }
+    }
+
+    // Get the rollbackOnly status of the transaction bound to the current thread at the time this method is called.
+    public boolean getRollbackOnly()
+    {
+        if (jtaLogger.logger.isTraceEnabled()) {
+            jtaLogger.logger.trace("TransactionSynchronizationRegistryImple.getRollbackOnly");
+        }
+
+        TransactionImple transactionImple = getTransactionImple();
+
+        if(transactionImple == null) {
+            throw new IllegalStateException();
+        }
+
+        try
+        {
+            return (transactionImple.getStatus() == Status.STATUS_MARKED_ROLLBACK);
+        }
+        catch (SystemException e)
+        {
+            throw new RuntimeException(jtaLogger.i18NLogger.get_transaction_arjunacore_systemexception(), e);
+        }
+    }
+
+    private TransactionImple getTransactionImple() throws IllegalStateException
+    {
+        TransactionImple transactionImple = null;
+        try
+        {
+            transactionImple = (TransactionImple)tm.getTransaction();
+        }
+        catch (SystemException e)
+        {
+            throw new RuntimeException(jtaLogger.i18NLogger.get_transaction_arjunacore_systemexception(), e);
+        }
+
+        if(transactionImple == null)
+        {
+            throw new IllegalStateException();
+        }
+
+        return transactionImple;
+    }
 }
--- ArjunaJTS/jtax/classes/com/arjuna/ats/internal/jta/transaction/jts/TransactionSynchronizationRegistryImple.java
+++ ArjunaJTS/jtax/classes/com/arjuna/ats/internal/jta/transaction/jts/TransactionSynchronizationRegistryImple.java
@@ -30,6 +30,8 @@ import javax.naming.Context;
 import javax.naming.Name;
 import javax.naming.spi.ObjectFactory;
 import javax.transaction.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.Serializable;
 import java.util.Hashtable;
 
@@ -49,179 +51,183 @@ public class TransactionSynchronizationRegistryImple implements TransactionSynch
          * http://jcp.org/aboutJava/communityprocess/maintenance/jsr907/907ChangeLog.html
          */
 
-        // Return an opaque object to represent the transaction bound to the current thread at the time this method is called.
+    // Return an opaque object to represent the transaction bound to the current thread at the time this method is called.
 
     private static final long serialVersionUID = 1L;
+
+    // cached for performance. Note: must set tm config before instantiating a TSRImple instance.
+    private transient javax.transaction.TransactionManager tm = com.arjuna.ats.jta.TransactionManager.transactionManager();
+
+    private void readObject(ObjectInputStream objectInputStream) throws IOException, ClassNotFoundException {
+        objectInputStream.defaultReadObject();
+        tm = com.arjuna.ats.jta.TransactionManager.transactionManager();
+    }
 
     public Object getObjectInstance(Object obj, Name name, Context nameCtx, Hashtable environment) throws Exception
     {
         return this;
     }
 
-        public Object getTransactionKey()
+    public Object getTransactionKey()
+    {
+        if (jtaxLogger.logger.isTraceEnabled()) {
+            jtaxLogger.logger.trace("TransactionSynchronizationRegistryImple.getTransactionKey");
+        }
+
+        TransactionImple transactionImple = null;
+        try
         {
-                if (jtaxLogger.logger.isTraceEnabled()) {
-                    jtaxLogger.logger.trace("TransactionSynchronizationRegistryImple.getTransactionKey");
-                }
-
-        javax.transaction.TransactionManager tm = com.arjuna.ats.jta.TransactionManager.transactionManager();
-                TransactionImple transactionImple = null;
-                try
-                {
-                        transactionImple = (TransactionImple)tm.getTransaction();
+            transactionImple = (TransactionImple)tm.getTransaction();
         }
-                catch (SystemException e)
-                {
-                        throw new RuntimeException(jtaxLogger.i18NLogger.get_jtax_transaction_jts_systemexception(), e);
-                }
-
-                if (transactionImple == null) {
-                        return null;
-                } else {
-                        return transactionImple.get_uid();
-                }
-        }
-
-        // Add or replace an object in the Map of resources being managed for the transaction bound to the current thread at the time this method is called.
-        public void putResource(Object key, Object value)
+        catch (SystemException e)
         {
-                if (jtaxLogger.logger.isTraceEnabled()) {
-                    jtaxLogger.logger.trace("TransactionSynchronizationRegistryImple.putResource");
-                }
-
-                if(key ==  null)
-                {
-                        throw new NullPointerException();
-                }
-
-                TransactionImple transactionImple = getTransactionImple();
-                transactionImple.putTxLocalResource(key, value);
+            throw new RuntimeException(jtaxLogger.i18NLogger.get_jtax_transaction_jts_systemexception(), e);
         }
 
-        // Get an object from the Map of resources being managed for the transaction bound to the current thread at the time this method is called.
-        public Object getResource(Object key)
+        if (transactionImple == null) {
+            return null;
+        } else {
+            return transactionImple.get_uid();
+        }
+    }
+
+    // Add or replace an object in the Map of resources being managed for the transaction bound to the current thread at the time this method is called.
+    public void putResource(Object key, Object value)
+    {
+        if (jtaxLogger.logger.isTraceEnabled()) {
+            jtaxLogger.logger.trace("TransactionSynchronizationRegistryImple.putResource");
+        }
+
+        if(key ==  null)
         {
-                if (jtaxLogger.logger.isTraceEnabled()) {
-                    jtaxLogger.logger.trace("TransactionSynchronizationRegistryImple.getResource");
-                }
-
-                if(key ==  null)
-                {
-                        throw new NullPointerException();
-                }
-
-                TransactionImple transactionImple = getTransactionImple();
-                return transactionImple.getTxLocalResource(key);
+            throw new NullPointerException();
         }
 
-        // Register a Synchronization instance with special ordering semantics.
-        public void registerInterposedSynchronization(Synchronization synchronization)
+        TransactionImple transactionImple = getTransactionImple();
+        transactionImple.putTxLocalResource(key, value);
+    }
+
+    // Get an object from the Map of resources being managed for the transaction bound to the current thread at the time this method is called.
+    public Object getResource(Object key)
+    {
+        if (jtaxLogger.logger.isTraceEnabled()) {
+            jtaxLogger.logger.trace("TransactionSynchronizationRegistryImple.getResource");
+        }
+
+        if(key ==  null)
         {
-                if (jtaxLogger.logger.isTraceEnabled()) {
-                    jtaxLogger.logger.trace("TransactionSynchronizationRegistryImple.registerInterposedSynchronization");
-                }
-
-                TransactionImple transactionImple = getTransactionImple();
-
-                try
-                {
-                        transactionImple.registerSynchronizationImple(new JTAInterposedSynchronizationImple(synchronization));
-                }
-                catch (RollbackException e)
-                {
-                        throw new com.arjuna.ats.jta.exceptions.RollbackException(jtaxLogger.i18NLogger.get_jtax_transaction_jts_syncrollbackexception(), e);
-                }
-                catch (SystemException e)
-                {
-                        throw new RuntimeException(jtaxLogger.i18NLogger.get_jtax_transaction_jts_systemexception(), e);
-                }
+            throw new NullPointerException();
         }
 
-        // Return the status of the transaction bound to the current thread at the time this method is called.
-        public int getTransactionStatus()
+        TransactionImple transactionImple = getTransactionImple();
+        return transactionImple.getTxLocalResource(key);
+    }
+
+    // Register a Synchronization instance with special ordering semantics.
+    public void registerInterposedSynchronization(Synchronization synchronization)
+    {
+        if (jtaxLogger.logger.isTraceEnabled()) {
+            jtaxLogger.logger.trace("TransactionSynchronizationRegistryImple.registerInterposedSynchronization");
+        }
+
+        TransactionImple transactionImple = getTransactionImple();
+
+        try
         {
-                if (jtaxLogger.logger.isTraceEnabled()) {
-                    jtaxLogger.logger.trace("TransactionSynchronizationRegistryImple.getTransactionStatus");
-                }
-
-                javax.transaction.TransactionManager tm = com.arjuna.ats.jta.TransactionManager.transactionManager();
-                try
-                {
-                        return tm.getStatus();
-                }
-                catch(SystemException e)
-                {
-                        throw new RuntimeException(jtaxLogger.i18NLogger.get_jtax_transaction_jts_systemexception(), e);
-                }
-
+            transactionImple.registerSynchronizationImple(new JTAInterposedSynchronizationImple(synchronization));
         }
-
-        // Set the rollbackOnly status of the transaction bound to the current thread at the time this method is called.
-        public void setRollbackOnly()
+        catch (RollbackException e)
         {
-                if (jtaxLogger.logger.isTraceEnabled()) {
-                    jtaxLogger.logger.trace("TransactionSynchronizationRegistryImple.setRollbackOnly");
-                }
-
-                javax.transaction.TransactionManager tm = com.arjuna.ats.jta.TransactionManager.transactionManager();
-                try
-                {
-                        Transaction transaction = tm.getTransaction();
-
-                        if(transaction == null)
-                        {
-                                throw new IllegalStateException();
-                        }
-
-                        tm.setRollbackOnly();
-                }
-                catch (SystemException e)
-                {
-                        throw new RuntimeException(jtaxLogger.i18NLogger.get_jtax_transaction_jts_systemexception(), e);
-                }
+            throw new com.arjuna.ats.jta.exceptions.RollbackException(jtaxLogger.i18NLogger.get_jtax_transaction_jts_syncrollbackexception(), e);
         }
-
-        // Get the rollbackOnly status of the transaction bound to the current thread at the time this method is called.
-        public boolean getRollbackOnly()
+        catch (SystemException e)
         {
-                if (jtaxLogger.logger.isTraceEnabled()) {
-                    jtaxLogger.logger.trace("TransactionSynchronizationRegistryImple.getRollbackOnly");
-                }
+            throw new RuntimeException(jtaxLogger.i18NLogger.get_jtax_transaction_jts_systemexception(), e);
+        }
+    }
 
-                TransactionImple transactionImple = getTransactionImple();
-
-                if(transactionImple == null) {
-                        throw new IllegalStateException();
-                }
-
-                try
-                {
-                        return (transactionImple.getStatus() == Status.STATUS_MARKED_ROLLBACK);
-                }
-                catch (SystemException e)
-                {
-                        throw new RuntimeException(jtaxLogger.i18NLogger.get_jtax_transaction_jts_systemexception(), e);
-                }
+    // Return the status of the transaction bound to the current thread at the time this method is called.
+    public int getTransactionStatus()
+    {
+        if (jtaxLogger.logger.isTraceEnabled()) {
+            jtaxLogger.logger.trace("TransactionSynchronizationRegistryImple.getTransactionStatus");
         }
 
-        private TransactionImple getTransactionImple() throws IllegalStateException
+        try
         {
-                javax.transaction.TransactionManager tm = com.arjuna.ats.jta.TransactionManager.transactionManager();
-                TransactionImple transactionImple = null;
-                try
-                {
-                        transactionImple = (TransactionImple)tm.getTransaction();
-                }
-                catch (SystemException e)
-                {
-                        throw new RuntimeException(jtaxLogger.i18NLogger.get_jtax_transaction_jts_systemexception(), e);
-                }
-
-                if(transactionImple == null)
-                {
-                        throw new IllegalStateException();
-                }
-
-                return transactionImple;
+            return tm.getStatus();
         }
+        catch(SystemException e)
+        {
+            throw new RuntimeException(jtaxLogger.i18NLogger.get_jtax_transaction_jts_systemexception(), e);
+        }
+
+    }
+
+    // Set the rollbackOnly status of the transaction bound to the current thread at the time this method is called.
+    public void setRollbackOnly()
+    {
+        if (jtaxLogger.logger.isTraceEnabled()) {
+            jtaxLogger.logger.trace("TransactionSynchronizationRegistryImple.setRollbackOnly");
+        }
+
+        try
+        {
+            Transaction transaction = tm.getTransaction();
+
+            if(transaction == null)
+            {
+                throw new IllegalStateException();
+            }
+
+            tm.setRollbackOnly();
+        }
+        catch (SystemException e)
+        {
+            throw new RuntimeException(jtaxLogger.i18NLogger.get_jtax_transaction_jts_systemexception(), e);
+        }
+    }
+
+    // Get the rollbackOnly status of the transaction bound to the current thread at the time this method is called.
+    public boolean getRollbackOnly()
+    {
+        if (jtaxLogger.logger.isTraceEnabled()) {
+            jtaxLogger.logger.trace("TransactionSynchronizationRegistryImple.getRollbackOnly");
+        }
+
+        TransactionImple transactionImple = getTransactionImple();
+
+        if(transactionImple == null) {
+            throw new IllegalStateException();
+        }
+
+        try
+        {
+            return (transactionImple.getStatus() == Status.STATUS_MARKED_ROLLBACK);
+        }
+        catch (SystemException e)
+        {
+            throw new RuntimeException(jtaxLogger.i18NLogger.get_jtax_transaction_jts_systemexception(), e);
+        }
+    }
+
+    private TransactionImple getTransactionImple() throws IllegalStateException
+    {
+        TransactionImple transactionImple = null;
+        try
+        {
+            transactionImple = (TransactionImple)tm.getTransaction();
+        }
+        catch (SystemException e)
+        {
+            throw new RuntimeException(jtaxLogger.i18NLogger.get_jtax_transaction_jts_systemexception(), e);
+        }
+
+        if(transactionImple == null)
+        {
+            throw new IllegalStateException();
+        }
+
+        return transactionImple;
+    }
 }
