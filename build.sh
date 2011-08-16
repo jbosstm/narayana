3c5047d4a6293b9dfff41519d762a47220ebb4cc
tomjenkinson <tomjenkinson@c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70>
2011-08-16 10:20:29 +0000

    added the options to configure orson and emma
    git-svn-id: https://anonsvn.jboss.org/repos/labs/labs/jbosstm/trunk@37416 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ MAVEN_SEARCH_PATH="\
     maven"
 
 # the default arguments
-MVN_OPTIONS="-s tools/maven/conf/settings.xml"
+MVN_OPTIONS="-s tools/maven/conf/settings.xml -Dorson.jar.location=`pwd`/ext/orson-0.5.0.jar -Demma.jar.location=`pwd`/ext/emma.jar"
 
 # Use the maximum available, or set MAX_FD != -1 to use that
 MAX_FD="maximum"
