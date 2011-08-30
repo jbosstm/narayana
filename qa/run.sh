9f220de5667cdfb1816bdb67a5946f8aef0c99a8
tomjenkinson <tomjenkinson@c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70>
2011-08-30 15:06:39 +0000

    updated to correct some minor upload issues
    git-svn-id: https://anonsvn.jboss.org/repos/labs/labs/jbosstm/trunk@37445 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- qa/run.sh
+++ qa/run.sh
@@ -2,4 +2,4 @@
 
 sed -i TaskImpl.properties -e "s#^COMMAND_LINE_0=.*#COMMAND_LINE_0=java#"
 ant -Ddriver.url=file:///home/tom/narayana/dbdrivers get.drivers dist
-ant -f run-tests.xml test -Dtest=jdbcresources01_oracle_thin_jndi
+ant -f run-tests.xml ci-tests
