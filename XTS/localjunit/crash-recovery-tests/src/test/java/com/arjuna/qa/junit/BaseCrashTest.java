6fed2a0b188f3a006ee338c11e0ebdfdb4ca0e9f
zhfeng <zhfeng@c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70>
2011-11-24 07:46:23 +0000

    JBTM-817 update to add runTest in BaseCrashTest
    git-svn-id: https://anonsvn.jboss.org/repos/labs/labs/jbosstm/trunk@37737 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- XTS/localjunit/crash-recovery-tests/src/test/java/com/arjuna/qa/junit/BaseCrashTest.java
+++ XTS/localjunit/crash-recovery-tests/src/test/java/com/arjuna/qa/junit/BaseCrashTest.java
@@ -2,16 +2,42 @@ package com.arjuna.qa.junit;
 
 import java.io.File;
 
+import org.jboss.arquillian.container.test.api.Config;
+import org.jboss.arquillian.container.test.api.ContainerController;
+import org.jboss.arquillian.container.test.api.Deployer;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.TargetsContainer;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.runner.RunWith;
 
+@RunWith(Arquillian.class)
 public class BaseCrashTest {
 	protected String XTSServiceTest = " -Dorg.jboss.jbossts.xts.servicetests.XTSServiceTestName=@TestName@";
 	protected String BytemanArgs = "-Djboss.modules.system.pkgs=org.jboss.byteman -Dorg.jboss.byteman.transform.all -javaagent:target/test-classes/lib/byteman.jar=script:target/test-classes/scripts/@BMScript@.txt,boot:target/test-classes/lib/byteman.jar,listener:true";
 	protected String javaVmArguments;
 	protected String testName;
 	protected String scriptName;
+	private final static String xtstestWar = "../../sar/tests/target/xtstest.war";
+
+	@ArquillianResource
+	private ContainerController controller;
+
+	@ArquillianResource
+	private Deployer deployer;
 	
+	@Deployment(name = "xtstest", testable = false, managed = false)
+	@TargetsContainer("jboss-as")
+	public static Archive<?> createTestArchive() {
+		WebArchive archive = ShrinkWrap.
+		createFromZipFile(WebArchive.class, new File(xtstestWar));
+		return archive;	
+	}
 	@Before
 	public void setUp() {
 		javaVmArguments = BytemanArgs.replace("@BMScript@", scriptName);
@@ -39,5 +65,26 @@ public class BaseCrashTest {
 				file.renameTo(new File(log+"/"+logFileName));
 			}
 		}
-	}	
+	}
+	
+	protected void runTest(String testClass) throws Exception {
+		Config config = new Config();
+		config.add("javaVmArguments", javaVmArguments + XTSServiceTest.replace("@TestName@", testClass));
+
+		controller.start("jboss-as", config.map());
+		deployer.deploy("xtstest");
+
+		//Waiting for crashing
+		Thread.sleep(2 * 60 * 1000);
+
+		//Boot jboss as after crashing
+		config.add("javaVmArguments", javaVmArguments);
+		controller.start("jboss-as", config.map());
+
+		//Waiting for recovery
+		Thread.sleep(5 * 60 * 1000);
+
+		deployer.undeploy("xtstest");
+		controller.stop("jboss-as");
+	}
 }
--- XTS/localjunit/crash-recovery-tests/src/test/java/com/arjuna/qa/junit/TestATCrashDuringOnePhaseCommit.java
+++ XTS/localjunit/crash-recovery-tests/src/test/java/com/arjuna/qa/junit/TestATCrashDuringOnePhaseCommit.java
@@ -1,63 +1,19 @@
 package com.arjuna.qa.junit;
 
-import java.io.File;
-
-import org.jboss.arquillian.container.test.api.Config;
-import org.jboss.arquillian.container.test.api.ContainerController;
-import org.jboss.arquillian.container.test.api.Deployer;
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.container.test.api.TargetsContainer;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.test.api.ArquillianResource;
-import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
 public class TestATCrashDuringOnePhaseCommit extends BaseCrashTest {
-	private final static String xtstestWar = "../../sar/tests/target/xtstest.war";
-
-	@ArquillianResource
-	private ContainerController controller;
-
-	@ArquillianResource
-	private Deployer deployer;
-	
 	public TestATCrashDuringOnePhaseCommit() {
 		scriptName ="ATCrashDuringOnePhaseCommit";
-	}
-
-	@Deployment(name = "xtstest", testable = false, managed = false)
-	@TargetsContainer("jboss-as")
-	public static Archive<?> createTestArchive() {
-		WebArchive archive = ShrinkWrap.
-		createFromZipFile(WebArchive.class, new File(xtstestWar));
-		return archive;	
 	}
 
 	@Test
 	public void SingleParticipantPrepareAndCommit() throws Exception {
 		testName = "SingleParticipantPrepareAndCommit";
 		String testClass = "org.jboss.jbossts.xts.servicetests.test.at.SingleParticipantPrepareAndCommitTest";
-		Config config = new Config();
-		config.add("javaVmArguments", javaVmArguments + XTSServiceTest.replace("@TestName@", testClass));
-
-		controller.start("jboss-as", config.map());
-		deployer.deploy("xtstest");
-
-		//Waiting for crash
-		Thread.sleep(2 * 60 * 1000);
-
-		//Boot jboss as after crashing
-		config.add("javaVmArguments", javaVmArguments);
-		controller.start("jboss-as", config.map());
-
-		//Waiting for recovery
-		Thread.sleep(5 * 60 * 1000);
-
-		deployer.undeploy("xtstest");
-		controller.stop("jboss-as");
+		runTest(testClass);
 	}
 }
