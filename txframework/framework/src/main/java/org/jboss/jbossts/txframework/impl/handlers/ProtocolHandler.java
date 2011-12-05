ca3e0358ca40e10bb56575d31f6cbd3266b5d278
paul.robinson <paul.robinson@c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70>
2011-12-05 11:53:19 +0000

    renamed package structure to org.jboss.narayana
    git-svn-id: https://anonsvn.jboss.org/repos/labs/labs/jbosstm/trunk@37774 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- txframework/framework/src/main/java/org/jboss/jbossts/txframework/impl/handlers/ProtocolHandler.java
+++
@@ -1,8 +0,0 @@
-package org.jboss.jbossts.txframework.impl.handlers;
-
-import javax.interceptor.InvocationContext;
-
-public interface ProtocolHandler
-{
-    public Object proceed(InvocationContext ic) throws Exception;
-}
