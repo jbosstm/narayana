ea5f4bf4148a304d61e4ef37d6e1187e9b46ec0a
Paul Robinson <paul.robinson@redhat.com>
2012-01-19 11:53:28 +0000

    Ensure object store is empty before each XTS crash-recovery test

--- XTS/sar/crash-recovery-tests/src/test/java/com/arjuna/qa/junit/BaseCrashTest.java
+++ XTS/sar/crash-recovery-tests/src/test/java/com/arjuna/qa/junit/BaseCrashTest.java
@@ -1,7 +1,6 @@
 package com.arjuna.qa.junit;
 
 import java.io.File;
-
 import org.jboss.arquillian.container.test.api.Config;
 import org.jboss.arquillian.container.test.api.ContainerController;
 import org.jboss.arquillian.container.test.api.Deployer;
@@ -14,85 +13,141 @@ import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Assert;
 
-public class BaseCrashTest {
-	protected String XTSServiceTest = " -Dorg.jboss.jbossts.xts.servicetests.XTSServiceTestName=@TestName@";
-	protected String BytemanArgs = "-Djboss.modules.system.pkgs=org.jboss.byteman -Dorg.jboss.byteman.transform.all -javaagent:target/test-classes/lib/byteman.jar=script:target/test-classes/scripts/@BMScript@.txt,boot:target/test-classes/lib/byteman.jar,listener:true";
-	protected String javaVmArguments;
-	protected String testName;
-	protected String scriptName;
-	private final static String xtstestWar = "../../sar/tests/target/xtstest.war";
+public class BaseCrashTest
+{
+    protected String XTSServiceTest = " -Dorg.jboss.jbossts.xts.servicetests.XTSServiceTestName=@TestName@";
+    protected String BytemanArgs = "-Djboss.modules.system.pkgs=org.jboss.byteman -Dorg.jboss.byteman.transform.all -javaagent:target/test-classes/lib/byteman.jar=script:target/test-classes/scripts/@BMScript@.txt,boot:target/test-classes/lib/byteman.jar,listener:true";
+    protected String javaVmArguments;
+    protected String testName;
+    protected String scriptName;
+    private final static String xtstestWar = "../tests/build/webapps/xtstest.war";
+    @ArquillianResource
+    private ContainerController controller;
+    @ArquillianResource
+    private Deployer deployer;
 
-	@ArquillianResource
-	private ContainerController controller;
+    @Deployment(name = "xtstest", testable = false, managed = false)
+    @TargetsContainer("jboss-as")
+    public static Archive<?> createTestArchive()
+    {
+        WebArchive archive = ShrinkWrap.
+                createFromZipFile(WebArchive.class, new File(xtstestWar));
+        final String ManifestMF = "Manifest-Version: 1.0\n"
+                + "Dependencies: org.jboss.modules,deployment.arquillian-service,org.jboss.msc,org.jboss.jts,org.jboss.xts\n";
+        archive.setManifest(new StringAsset(ManifestMF));
 
-	@ArquillianResource
-	private Deployer deployer;
-	
-	@Deployment(name = "xtstest", testable = false, managed = false)
-	@TargetsContainer("jboss-as")
-	public static Archive<?> createTestArchive() {
-		WebArchive archive = ShrinkWrap.
-		createFromZipFile(WebArchive.class, new File(xtstestWar));
-		final String ManifestMF = "Manifest-Version: 1.0\n"
-			+ "Dependencies: org.jboss.modules,deployment.arquillian-service,org.jboss.msc,org.jboss.jts,org.jboss.xts\n";
-		archive.setManifest(new StringAsset(ManifestMF));
+        return archive;
+    }
 
-		return archive;	
-	}
-	@Before
-	public void setUp() {
-		javaVmArguments = BytemanArgs.replace("@BMScript@", scriptName);
-		
-		File file = new File("testlog");
-		if(file.isFile() && file.exists()){
-			file.delete();
-		}
-	}
-	
-	@After
-	public void tearDown() {
-		String log = "target/log";
+    @Before
+    public void setUp()
+    {
+        javaVmArguments = BytemanArgs.replace("@BMScript@", scriptName);
 
-		if(testName != null && scriptName != null) {
-			String logFileName = scriptName + "." + testName;
-			File file = new File("testlog");
-			File logDir = new File(log);
+        File file = new File("testlog");
+        if (file.isFile() && file.exists())
+        {
+            file.delete();
+        }
 
-			if(!logDir.exists()) {
-				logDir.mkdirs();
-			}
+        //Ensure ObjectStore is empty:
+        String jbossHome = System.getenv("JBOSS_HOME");
+        if (jbossHome == null)
+        {
+            Assert.fail("$JBOSS_HOME not set");
+        }
+        else
+        {
+            File objectStore = new File(jbossHome + File.separator + "standalone" + File.separator + "data" + File.separator + "tx-object-store");
+            System.out.println("Deleting: " + objectStore.getPath());
 
-			if(file.isFile() && file.exists()){
-				file.renameTo(new File(log+"/"+logFileName));
-			}
-		}
-	}
+            if (objectStore.exists())
+            {
+                boolean success = deleteDirectory(objectStore);
+                if (!success)
+                {
+                    System.err.println("Failed to remove tx-object-store");
+                    Assert.fail("Failed to remove tx-object-store: " + objectStore.getPath());
+                }
+                else
+                {
+                    System.out.println("remove tx-object-store: " + objectStore.getPath());
+                }
+            }
 
-	protected void runTest(String testClass) throws Exception {
-		Config config = new Config();
-		config.add("javaVmArguments", javaVmArguments + XTSServiceTest.replace("@TestName@", testClass));
+        }
+    }
 
-		controller.start("jboss-as", config.map());
-		deployer.undeploy("xtstest");
-		deployer.deploy("xtstest");
+    @After
+    public void tearDown()
+    {
+        String log = "target/log";
 
-		//Waiting for crashing
-		controller.kill("jboss-as");
+        if (testName != null && scriptName != null)
+        {
+            String logFileName = scriptName + "." + testName;
+            File file = new File("testlog");
+            File logDir = new File(log);
 
-		//Boot jboss-as after crashing
-		config.add("javaVmArguments", javaVmArguments);
-		controller.start("jboss-as", config.map());
-		
-		//redeploy xtstest
-		deployer.undeploy("xtstest");
-		deployer.deploy("xtstest");
+            if (!logDir.exists())
+            {
+                logDir.mkdirs();
+            }
 
-		//Waiting for recovery
-		//Thread.sleep(waitForRecovery * 60 * 1000);
+            if (file.isFile() && file.exists())
+            {
+                file.renameTo(new File(log + "/" + logFileName));
+            }
+        }
+    }
 
-		//deployer.undeploy("xtstest");
-		//controller.stop("jboss-as");
-		controller.kill("jboss-as");
-	}
+    protected void runTest(String testClass) throws Exception
+    {
+        Config config = new Config();
+        config.add("javaVmArguments", javaVmArguments + XTSServiceTest.replace("@TestName@", testClass));
+
+        controller.start("jboss-as", config.map());
+        deployer.undeploy("xtstest");
+        deployer.deploy("xtstest");
+
+        //Waiting for crashing
+        controller.kill("jboss-as");
+
+        //Boot jboss-as after crashing
+        config.add("javaVmArguments", javaVmArguments);
+        controller.start("jboss-as", config.map());
+
+        //redeploy xtstest
+        deployer.undeploy("xtstest");
+        deployer.deploy("xtstest");
+
+        //Waiting for recovery
+        //Thread.sleep(waitForRecovery * 60 * 1000);
+
+        //deployer.undeploy("xtstest");
+        //controller.stop("jboss-as");
+        controller.kill("jboss-as");
+    }
+
+    private boolean deleteDirectory(File path)
+    {
+        if (path.exists())
+        {
+            File[] files = path.listFiles();
+            for (int i = 0; i < files.length; i++)
+            {
+                if (files[i].isDirectory())
+                {
+                    deleteDirectory(files[i]);
+                }
+                else
+                {
+                    files[i].delete();
+                }
+            }
+        }
+        return (path.delete());
+    }
 }
