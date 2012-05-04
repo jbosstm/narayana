61ce31d5f3e3b557c1856869afb73d06b1c46013
Tom Jenkinson <tom.jenkinson@redhat.com>
2012-05-04 15:30:36 +0100

    JBTM-1155 updated to add a test for HeuristicRollback

--- ArjunaJTA/jta/tests/classes/com/hp/mwtests/ts/jta/recovery/CrashRecoveryCommitReturnsXA_RETRY.java
+++ ArjunaJTA/jta/tests/classes/com/hp/mwtests/ts/jta/recovery/CrashRecoveryCommitReturnsXA_RETRY.java
@@ -45,10 +45,16 @@ import org.jboss.byteman.contrib.bmunit.BMUnitRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import com.arjuna.ats.arjuna.AtomicAction;
 import com.arjuna.ats.arjuna.common.RecoveryEnvironmentBean;
+import com.arjuna.ats.arjuna.common.Uid;
+import com.arjuna.ats.arjuna.objectstore.StoreManager;
 import com.arjuna.ats.arjuna.recovery.RecoveryManager;
 import com.arjuna.ats.arjuna.recovery.RecoveryModule;
+import com.arjuna.ats.arjuna.state.InputObjectState;
+import com.arjuna.ats.internal.arjuna.common.UidHelper;
 import com.arjuna.ats.internal.jta.recovery.arjunacore.XARecoveryModule;
+import com.arjuna.ats.internal.jta.transaction.arjunacore.TransactionImple;
 import com.arjuna.ats.jta.recovery.XAResourceRecoveryHelper;
 import com.arjuna.common.internal.util.propertyservice.BeanPopulator;
 
@@ -67,8 +73,8 @@ public class CrashRecoveryCommitReturnsXA_RETRY {
 
 		RecoveryEnvironmentBean recoveryEnvironmentBean = BeanPopulator
 				.getDefaultInstance(RecoveryEnvironmentBean.class);
-		recoveryEnvironmentBean.setRecoveryBackoffPeriod(3);
-		recoveryEnvironmentBean.setPeriodicRecoveryPeriod(3);
+		recoveryEnvironmentBean.setRecoveryBackoffPeriod(1);
+		recoveryEnvironmentBean.setPeriodicRecoveryPeriod(1);
 
 		List<String> recoveryModuleClassNames = new ArrayList<String>();
 
@@ -143,5 +149,118 @@ public class CrashRecoveryCommitReturnsXA_RETRY {
 			toWakeUp.wait();
 		}
 		assertTrue(secondResource.wasCommitted());
+	}
+
+	@Test
+	public void testHeuristicRollback() throws Exception {
+		// this test is supposed to leave a record around in the log store
+		// during a commit long enough
+		// that the periodic recovery thread runs and detects it. rather than
+		// rely on delays to make
+		// this happen (placing us at the mercy of the scheduler) we use a
+		// byteman script to enforce
+		// the thread sequence we need
+
+		RecoveryEnvironmentBean recoveryEnvironmentBean = BeanPopulator
+				.getDefaultInstance(RecoveryEnvironmentBean.class);
+		recoveryEnvironmentBean.setRecoveryBackoffPeriod(1);
+		recoveryEnvironmentBean.setPeriodicRecoveryPeriod(1);
+
+		List<String> recoveryModuleClassNames = new ArrayList<String>();
+
+		recoveryModuleClassNames
+				.add("com.arjuna.ats.internal.arjuna.recovery.AtomicActionRecoveryModule");
+		recoveryModuleClassNames
+				.add("com.arjuna.ats.internal.jta.recovery.arjunacore.XARecoveryModule");
+		recoveryEnvironmentBean
+				.setRecoveryModuleClassNames(recoveryModuleClassNames);
+		List<String> expiryScannerClassNames = new ArrayList<String>();
+		expiryScannerClassNames
+				.add("com.arjuna.ats.internal.arjuna.recovery.ExpiredTransactionStatusManagerScanner");
+		recoveryEnvironmentBean
+				.setExpiryScannerClassNames(expiryScannerClassNames);
+		recoveryEnvironmentBean.setRecoveryActivators(null);
+		// start the recovery manager
+
+		RecoveryManager.manager().initialize();
+
+		XARecoveryModule xaRecoveryModule = null;
+		for (RecoveryModule recoveryModule : ((Vector<RecoveryModule>) RecoveryManager
+				.manager().getModules())) {
+			if (recoveryModule instanceof XARecoveryModule) {
+				xaRecoveryModule = (XARecoveryModule) recoveryModule;
+				break;
+			}
+		}
+
+		if (xaRecoveryModule == null) {
+			throw new Exception("No XARM");
+		}
+
+		XAResource firstResource = new SimpleResource();
+		Object toWakeUp = new Object();
+		final SimpleResourceXA_RETRYHeuristicRollback secondResource = new SimpleResourceXA_RETRYHeuristicRollback(
+				toWakeUp);
+
+		xaRecoveryModule
+				.addXAResourceRecoveryHelper(new XAResourceRecoveryHelper() {
+
+					@Override
+					public boolean initialise(String p) throws Exception {
+						// TODO Auto-generated method stub
+						return true;
+					}
+
+					@Override
+					public XAResource[] getXAResources() throws Exception {
+						// TODO Auto-generated method stub
+						return new XAResource[] { secondResource };
+					}
+				});
+
+		// ok, now drive a TX to completion. the script should ensure that the
+		// recovery
+
+		javax.transaction.TransactionManager tm = com.arjuna.ats.jta.TransactionManager
+				.transactionManager();
+
+		tm.begin();
+
+		javax.transaction.Transaction theTransaction = tm.getTransaction();
+		Uid txUid = ((TransactionImple) theTransaction).get_uid();
+
+		theTransaction.enlistResource(firstResource);
+		theTransaction.enlistResource(secondResource);
+
+		assertFalse(secondResource.wasCommitted());
+
+		tm.commit();
+
+		synchronized (toWakeUp) {
+			toWakeUp.wait();
+		}
+		assertTrue(secondResource.wasCommitted());
+
+		InputObjectState uids = new InputObjectState();
+		StoreManager.getRecoveryStore().allObjUids(
+				"/StateManager/BasicAction/TwoPhaseCoordinator/AtomicAction",
+				uids);
+		boolean moreUids = true;
+
+		boolean found = false;
+		while (moreUids) {
+			Uid theUid = UidHelper.unpackFrom(uids);
+			if (theUid.equals(txUid)) {
+				AtomicAction aa = new AtomicAction(theUid);
+				System.out.println(aa.status());
+				found = true;
+			} else if (theUid.equals(Uid.nullUid())) {
+				moreUids = false;
+			}
+		}
+		
+		if (!found) {
+			throw new Exception("Could not locate the Uid");
+		}
 	}
 }
