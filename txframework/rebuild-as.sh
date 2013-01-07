34ccf6e444c50a6921ab38059941a5604ae1b848
Paul Robinson <paul.robinson@redhat.com>
2013-01-07 16:51:02 +0000

    JBTM-1293 ATBridgeTest#testSimple counter isn't incremented

--- txframework/rebuild-as.sh
+++ txframework/rebuild-as.sh
@@ -14,10 +14,15 @@ if [ "" == "$JBOSS_BUILD_HOME" ]; then
     exit 1
 fi
 
-if [ "$2" == "--clean" ]; then
+if [ "$2" == "--clean" ] || [ "$3" == "--clean" ]; then
     CLEAN=clean
 fi
 
+if [ "$2" == "--xts" ] || [ "$3" == "--xts" ]; then
+    cd ..
+    ./build.sh -f ./XTS/pom.xml $CLEAN install -DskipTests || fail
+    cd -
+fi
 
 mvn $CLEAN install || fail
 kill-jboss
@@ -27,7 +32,7 @@ $JBOSS_BUILD_HOME/build.sh $CLEAN install -f $JBOSS_BUILD_HOME/build/pom.xml -Ds
 JBOSS_BUILD_NAME=$(ls $JBOSS_BUILD_HOME/build/target/ | grep jboss-as)
 cp $JBOSS_BUILD_HOME/build/target/$JBOSS_BUILD_NAME/docs/examples/configs/standalone-xts.xml $JBOSS_BUILD_HOME/build/target/$JBOSS_BUILD_NAME/standalone/configuration/ || fail
 echo JAVA_OPTS=\"\$JAVA_OPTS -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n\" >> $JBOSS_BUILD_HOME/build/target/$JBOSS_BUILD_NAME/bin/standalone.conf
-echo JAVA_OPTS=\"\$JAVA_OPTS -Dorg.jboss.byteman.verbose -Djboss.modules.system.pkgs=org.jboss.byteman -Dorg.jboss.byteman.transform.all -javaagent:/opt/byteman-download-2.1.0/lib/byteman.jar=listener:true\" >> $JBOSS_BUILD_HOME/build/target/$JBOSS_BUILD_NAME/bin/standalone.conf
+echo JAVA_OPTS=\"\$JAVA_OPTS -Dorg.jboss.byteman.verbose -Djboss.modules.system.pkgs=org.jboss.byteman -Dorg.jboss.byteman.transform.all -javaagent:/opt/byteman-download-2.1.2/lib/byteman.jar=listener:true\" >> $JBOSS_BUILD_HOME/build/target/$JBOSS_BUILD_NAME/bin/standalone.conf
 cp ../rest-tx/webservice/target/restat-web-*.war $JBOSS_BUILD_HOME/build/target/$JBOSS_BUILD_NAME/standalone/deployments/
 
 END=$(date +%s)
