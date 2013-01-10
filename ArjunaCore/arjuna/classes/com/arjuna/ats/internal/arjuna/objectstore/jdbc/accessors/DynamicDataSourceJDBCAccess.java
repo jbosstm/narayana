41a0bb8fc72744660c05a3da38c4602fdb07e0c0
Tom Jenkinson <tom.jenkinson@redhat.com>
2013-01-10 12:51:50 +0000

    JBTM-1406 updated to only run with the JDBC store for the minimum number of tests as that is not the best default, also updated to set h2 to not close on exit to prevent race condition in TransactionStatusManager shutdown before db closed

--- ArjunaCore/arjuna/classes/com/arjuna/ats/internal/arjuna/objectstore/jdbc/accessors/DynamicDataSourceJDBCAccess.java
+++ ArjunaCore/arjuna/classes/com/arjuna/ats/internal/arjuna/objectstore/jdbc/accessors/DynamicDataSourceJDBCAccess.java
@@ -59,7 +59,7 @@ public class DynamicDataSourceJDBCAccess implements JDBCAccess {
 			Map<String, String> configuration = new HashMap<String, String>();
 			while (tokenizer.hasMoreTokens()) {
 				String[] split = tokenizer.nextToken().split("=");
-				configuration.put(split[0], split[1]);
+				configuration.put(split[0], split[1].replace("\\equ", "="));
 			}
 			try {
 				this.dataSource = (DataSource) Class.forName(
@@ -72,7 +72,8 @@ public class DynamicDataSourceJDBCAccess implements JDBCAccess {
 					try {
 						method = dataSource.getClass().getMethod("set" + key,
 								java.lang.String.class);
-						method.invoke(dataSource, value);
+						String replace = value.replace("\\semi", ";");
+						method.invoke(dataSource, value.replace("\\semi", ";"));
 					} catch (NoSuchMethodException nsme) {
 						method = dataSource.getClass().getMethod("set" + key,
 								int.class);
