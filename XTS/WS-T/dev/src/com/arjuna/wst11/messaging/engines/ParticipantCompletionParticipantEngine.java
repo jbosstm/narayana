1fa9d41e685713c7323222a0bb83e41ec1fd3d31
Gytis Trikleris <gytist@gmail.com>
2013-01-11 15:30:26 +0000

    JBTM-1031. == replaced with equals() in objects comparison where appropriate

--- XTS/WS-T/dev/src/com/arjuna/wst11/messaging/engines/ParticipantCompletionParticipantEngine.java
+++ XTS/WS-T/dev/src/com/arjuna/wst11/messaging/engines/ParticipantCompletionParticipantEngine.java
@@ -712,7 +712,7 @@ public class ParticipantCompletionParticipantEngine implements ParticipantComple
         final State current ;
         synchronized(this)
         {
-            if (timerTask != caller) {
+            if (!timerTask.equals(caller)) {
                 // the timer was cancelled but it went off before it could be cancelled
 
                 return;
