ec5e147b25daa2aee97fd3f0c2f354ac4411c980
mmusgrov <mmusgrov@redhat.com>
2013-02-13 20:36:28 +0000

    JBTM-1466 Use a predeined record type id for RESTAT records

--- rest-tx/tx/src/main/java/org/jboss/jbossts/star/resource/RESTRecord.java
+++ rest-tx/tx/src/main/java/org/jboss/jbossts/star/resource/RESTRecord.java
@@ -103,7 +103,7 @@ public class RESTRecord extends AbstractRecord
 
     public int typeIs()
     {
-        return RecordType.USER_DEF_FIRST0;
+        return RecordType.RESTAT_RECORD;
     }
 
     public Object value()
