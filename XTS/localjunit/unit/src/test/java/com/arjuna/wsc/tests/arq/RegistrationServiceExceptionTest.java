e3ea4f73954c4ba7482bf45c685895ab1927c50f
Paul Robinson <paul.robinson@redhat.com>
2013-04-17 08:50:14 +0100

    JBTM-1637 disable tests until fixed

--- XTS/localjunit/unit/src/test/java/com/arjuna/wsc/tests/arq/RegistrationServiceExceptionTest.java
+++ XTS/localjunit/unit/src/test/java/com/arjuna/wsc/tests/arq/RegistrationServiceExceptionTest.java
@@ -29,6 +29,7 @@ import javax.xml.ws.wsaddressing.W3CEndpointReference;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.oasis_open.docs.ws_tx.wscoor._2006._06.CoordinationContextType;
@@ -50,6 +51,7 @@ public class RegistrationServiceExceptionTest extends BaseWSCTest {
     }
 
     @Test
+    @Ignore //JBTM-1637
     public void testAlreadyRegisteredProtocolIdentifierException()
             throws Exception
             {
@@ -74,6 +76,7 @@ public class RegistrationServiceExceptionTest extends BaseWSCTest {
             }
 
     @Test
+    @Ignore //JBTM-1637
     public void testInvalidProtocolProtocolIdentifierException()
             throws Exception
             {
@@ -98,6 +101,7 @@ public class RegistrationServiceExceptionTest extends BaseWSCTest {
             }
 
     @Test
+    @Ignore //JBTM-1637
     public void testInvalidStateProtocolIdentifierException()
             throws Exception
             {
@@ -122,6 +126,7 @@ public class RegistrationServiceExceptionTest extends BaseWSCTest {
             }
 
     @Test
+    @Ignore //JBTM-1637
     public void testNoActivityProtocolIdentifierException()
             throws Exception
             {
