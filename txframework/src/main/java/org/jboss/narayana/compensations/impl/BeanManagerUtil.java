86bd4aa51b97ff120d97c03b04b27052bcacaba5
Paul Robinson <paul.robinson@redhat.com>
2013-05-24 10:38:17 +0100

    JBTM-1710 CDI @Inject doesn't work in CompensationHandler et al

---
+++ txframework/src/main/java/org/jboss/narayana/compensations/impl/BeanManagerUtil.java
@@ -0,0 +1,40 @@
+package org.jboss.narayana.compensations.impl;
+
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.lang.reflect.Type;
+import java.util.Iterator;
+
+public class BeanManagerUtil {
+
+
+    public static BeanManager getBeanManager() {
+
+        try {
+            return InitialContext.doLookup("java:comp/BeanManager");
+        } catch (NamingException e) {
+            //Do nothing, and move onto alternative
+        }
+
+        try {
+            return InitialContext.doLookup("java:comp/env/BeanManager");
+        } catch (NamingException e) {
+            throw new RuntimeException("BeanManager not available in JNDI");
+        }
+    }
+
+    public static <T> T createBeanInstance(Class<T> clazz, BeanManager bm) {
+
+        Iterator<Bean<?>> iter = bm.getBeans(clazz).iterator();
+        if (!iter.hasNext()) {
+            throw new IllegalStateException("CDI BeanManager cannot find an instance of requested type " + clazz.getName());
+        }
+        Bean<T> bean = (Bean<T>) iter.next();
+        CreationalContext<T> ctx = bm.createCreationalContext(bean);
+        return(T) bm.getReference(bean, clazz, ctx);
+    }
+
+}
