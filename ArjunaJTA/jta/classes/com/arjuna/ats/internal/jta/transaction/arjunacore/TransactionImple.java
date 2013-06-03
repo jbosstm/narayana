8a61a06b2fc7ba7bf6223c1717ef18a63c558d56
Tom Jenkinson <tom.jenkinson@redhat.com>
2013-06-03 11:36:49 +0100

    JBTM-1636 Updated to print out the string form of the status when tracing is on

--- ArjunaJTA/jta/classes/com/arjuna/ats/internal/jta/transaction/arjunacore/TransactionImple.java
+++ ArjunaJTA/jta/classes/com/arjuna/ats/internal/jta/transaction/arjunacore/TransactionImple.java
@@ -65,6 +65,7 @@ import com.arjuna.ats.jta.exceptions.InactiveTransactionException;
 import com.arjuna.ats.jta.exceptions.InvalidTerminationStateException;
 import com.arjuna.ats.jta.logging.jtaLogger;
 import com.arjuna.ats.jta.resources.LastResourceCommitOptimisation;
+import com.arjuna.ats.jta.utils.JTAHelper;
 import com.arjuna.ats.jta.utils.XAHelper;
 import com.arjuna.ats.jta.xa.XAModifier;
 import com.arjuna.ats.jta.xa.XidImple;
@@ -328,8 +329,12 @@ public class TransactionImple implements javax.transaction.Transaction,
 		
 		if (_theTransaction != null)
 		{
-			return StatusConverter.convert(_theTransaction.status());
+			status = StatusConverter.convert(_theTransaction.status());
 		}
+		
+		if (jtaLogger.logger.isTraceEnabled()) {
+            jtaLogger.logger.trace("TransactionImple.getStatus: " + JTAHelper.stringForm(status));
+        }
 
 		return status;
 	}
