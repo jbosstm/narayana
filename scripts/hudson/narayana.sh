a561a4d5989edce02831e69c614ecbb276627d37
Tom Jenkinson <tom.jenkinson@redhat.com>
2013-06-03 10:48:23 +0100

    JBTM-1691 Don't attempt to auto merge the conflicts

--- scripts/hudson/narayana.sh
+++ scripts/hudson/narayana.sh
@@ -90,14 +90,6 @@ function build_as {
   echo $(git rev-parse upstream/master)
 
   git pull --rebase --ff-only upstream master
-  while [ $? != 0 ]
-  do
-     for i in `git status -s | sed "s/UU \(.*\)/\1/g"`
-     do 
-        awk '/^<+ HEAD$/,/^=+$/{next} /^>+ /{next} 1' $i > $i.bak; mv $i.bak $i; git add $i
-     done
-     git rebase --continue
-  done
   [ $? = 0 ] || fatal "git rebase failed"
 
   export MAVEN_OPTS="$MAVEN_OPTS -XX:MaxPermSize=512m"
