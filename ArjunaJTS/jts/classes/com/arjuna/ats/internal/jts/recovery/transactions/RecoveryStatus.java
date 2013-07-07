78fb6cd4247cda3bde996e4a7036ab947721776a
Mark Little <mlittle@redhat.com>
2013-07-07 17:36:45 +0100

    Added print routine to RecoveryStatus.

--- ArjunaJTS/jts/classes/com/arjuna/ats/internal/jts/recovery/transactions/RecoveryStatus.java
+++ ArjunaJTS/jts/classes/com/arjuna/ats/internal/jts/recovery/transactions/RecoveryStatus.java
@@ -39,11 +39,36 @@ package com.arjuna.ats.internal.jts.recovery.transactions;
 
 public class RecoveryStatus
 {
-    public static int NEW = 0;
-    public static int ACTIVATED = 1;
-    public static int ACTIVATE_FAILED = 2;
-    public static int REPLAYING = 3;
-    public static int REPLAYED = 4;
-    public static int REPLAY_FAILED = 5;
+    public static final int NEW = 0;
+    public static final int ACTIVATED = 1;
+    public static final int ACTIVATE_FAILED = 2;
+    public static final int REPLAYING = 3;
+    public static final int REPLAYED = 4;
+    public static final int REPLAY_FAILED = 5; 
+    
+    /**
+     * @return <code>String</code> representation of the status.
+     */
+
+    public static String stringForm (int res)
+    {
+        switch (res)
+        {
+        case NEW:
+            return "RecoveryStatus.NEW";
+        case ACTIVATED:
+            return "RecoveryStatus.ACTIVATED";
+        case ACTIVATE_FAILED:
+            return "RecoveryStatus.ACTIVATE_FAILED";
+        case REPLAYING:
+            return "RecoveryStatus.REPLAYING";
+        case REPLAYED:
+            return "RecoveryStatus.REPLAYED";
+        case REPLAY_FAILED:
+            return "RecoveryStatus.REPLAY_FAILED";
+        default:
+            return "Unknown";
+        }
+    }
 }
 
