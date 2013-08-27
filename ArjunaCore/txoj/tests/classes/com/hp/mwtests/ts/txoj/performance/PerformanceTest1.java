d19eb44edb05828fa900ef14f5315bed12ba165c
Michael Musgrove <mmusgrov@redhat.com>
2013-08-27 17:50:17 +0100

    [JBTM-1464] report results of txoj performance tests

--- ArjunaCore/txoj/tests/classes/com/hp/mwtests/ts/txoj/performance/PerformanceTest1.java
+++ ArjunaCore/txoj/tests/classes/com/hp/mwtests/ts/txoj/performance/PerformanceTest1.java
@@ -40,7 +40,7 @@ import com.hp.mwtests.ts.txoj.common.exceptions.TestException;
 import com.hp.mwtests.ts.txoj.common.resources.AtomicObject;
 import com.hp.mwtests.ts.txoj.common.resources.RecoverableObject;
 
-public class PerformanceTest1
+public class PerformanceTest1 extends PerformanceTestBase
 {
     @Test
     public void recoverableTest()
@@ -59,6 +59,8 @@ public class PerformanceTest1
         }
 
         A.commit();
+
+        reportThroughput("recoverableTest", iters, t1);
     }
 
     @Test
@@ -84,5 +86,7 @@ public class PerformanceTest1
         }
 
         A.commit();
+
+        reportThroughput("persistentTest", iters, t1);
     }
 }
