df4167bf2b04818a56220db3f66418a73375ed94
Alex Creasy <arcreasy@gmail.com>
2013-09-12 17:02:24 +0100

    JBTM-1915 Enhancement to logging to output the status of prepare, commit and abort for each resource after they complete

--- ArjunaCore/arjuna/classes/com/arjuna/ats/arjuna/coordinator/BasicAction.java
+++ ArjunaCore/arjuna/classes/com/arjuna/ats/arjuna/coordinator/BasicAction.java
@@ -1520,6 +1520,13 @@ public class BasicAction extends StateManager
             }
         }
 
+        if (tsLogger.logger.isTraceEnabled()) {
+            tsLogger.logger.tracef("BasicAction::End() result for action-id (%s) is (%s) node id: (%s)",
+                    get_uid(),
+                    TwoPhaseOutcome.stringForm(actionStatus),
+                    arjPropertyManager.getCoreEnvironmentBean().getNodeIdentifier());
+        }
+
         boolean returnCurrentStatus = false;
 
         if (reportHeuristics || (!reportHeuristics && !TxControl.asyncCommit))
@@ -2515,6 +2522,13 @@ public class BasicAction extends StateManager
         p = ((actionType == ActionType.TOP_LEVEL) ? record.topLevelPrepare()
                 : record.nestedPrepare());
 
+        if (tsLogger.logger.isTraceEnabled()) {
+            tsLogger.logger.tracef(
+                    "BasicAction::doPrepare() result for action-id (%s) on record id: (%s) is (%s) node id: (%s)",
+                    get_uid(), record.order(), TwoPhaseOutcome.stringForm(p),
+                    arjPropertyManager.getCoreEnvironmentBean().getNodeIdentifier());
+        }
+
         if (p == TwoPhaseOutcome.PREPARE_OK)
         {
             record = insertRecord(preparedList, record);
@@ -2813,6 +2827,14 @@ public class BasicAction extends StateManager
             {
                 /* Preserve error messages */
             }
+
+            if (tsLogger.logger.isTraceEnabled()) {
+                tsLogger.logger.tracef(
+                        "BasicAction::doCommit() result for action-id (%s) on record id: (%s) is (%s) node id: (%s)",
+                        get_uid(), record.order(), TwoPhaseOutcome.stringForm(ok),
+                        arjPropertyManager.getCoreEnvironmentBean().getNodeIdentifier());
+            }
+
         }
 
         return ok;
@@ -2905,6 +2927,14 @@ public class BasicAction extends StateManager
 
                 recordBeingHandled = null;
             }
+
+            if (tsLogger.logger.isTraceEnabled()) {
+                tsLogger.logger.tracef(
+                        "BasicAction::doAbort() result for action-id (%s) on record id: (%s) is (%s) node id: (%s)",
+                        get_uid(), record.order(), TwoPhaseOutcome.stringForm(ok),
+                        arjPropertyManager.getCoreEnvironmentBean().getNodeIdentifier());
+            }
+
         }
 
         return ok;
