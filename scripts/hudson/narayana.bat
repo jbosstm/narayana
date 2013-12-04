cefab773e6bc7b49019abf182fe8e5865e45a882
Tom Jenkinson <tom.jenkinson@redhat.com>
2013-12-04 12:38:40 +0000

    Wildfly have added a wildfly-core to their build folder so disambiguate

--- scripts/hudson/narayana.bat
+++ scripts/hudson/narayana.bat
@@ -23,7 +23,8 @@ call build.bat -f blacktie\wildfly-blacktie\pom.xml clean install || (call:comme
 echo "Building BlackTie
 cd blacktie
 rmdir wildfly-%WILDFLY_MASTER_VERSION% /s /q
-xcopy ..\jboss-as\build\target\wildfly-%WILDFLY_MASTER_VERSION% . /S
+mkdir wildfly-%WILDFLY_MASTER_VERSION%
+xcopy ..\jboss-as\build\target\wildfly-%WILDFLY_MASTER_VERSION% wildfly-%WILDFLY_MASTER_VERSION% /S
 set JBOSS_HOME=%CD%\wildfly-%WILDFLY_MASTER_VERSION%\
 unzip wildfly-blacktie\build\target\wildfly-blacktie-build-%WILDFLY_MASTER_VERSION%-bin.zip -d %JBOSS_HOME%
 cd ..\
