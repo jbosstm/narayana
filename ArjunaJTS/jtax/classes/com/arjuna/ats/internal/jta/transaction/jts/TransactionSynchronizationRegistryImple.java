1e32ffdc889cbe6ba377a244a742bade79a9103e
Tom Jenkinson <tom.jenkinson@redhat.com>
2013-12-04 10:00:37 +0000

    JBTM-2040 Removed additional line of debug around getTransactionStatus as BaseTransaction logs this immediately anyway

--- ArjunaJTS/jtax/classes/com/arjuna/ats/internal/jta/transaction/jts/TransactionSynchronizationRegistryImple.java
+++ ArjunaJTS/jtax/classes/com/arjuna/ats/internal/jta/transaction/jts/TransactionSynchronizationRegistryImple.java
@@ -154,10 +154,6 @@ public class TransactionSynchronizationRegistryImple implements TransactionSynch
     // Return the status of the transaction bound to the current thread at the time this method is called.
     public int getTransactionStatus()
     {
-        if (jtaxLogger.logger.isTraceEnabled()) {
-            jtaxLogger.logger.trace("TransactionSynchronizationRegistryImple.getTransactionStatus");
-        }
-
         try
         {
             return tm.getStatus();
