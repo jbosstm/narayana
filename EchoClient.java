21ad950567669eaae93fbbb907aa5e2ca75e3e1e
Mark Little <mlittle@redhat.com>
2013-12-10 11:30:54 +0000

    Updated comment.

--- EchoClient.java
+++ EchoClient.java
@@ -92,6 +92,9 @@ public class EchoClient extends Verticle {
 	   * address spaces then go into the ObjectStore dir and look for the Uid that represents the state
 	   * you want to share. Then uncomment the Uid line below and replace the Uid in quotes with the Uid
 	   * you have selected. Uncomment the other obj1 creation line and comment out the original.
+	   *
+	   * If you want to see how this might work then just go with the example state in the ObjectStore
+	   * shipped as part of this example and uncomment the lines.
 	   */
 	  
 	  /*
