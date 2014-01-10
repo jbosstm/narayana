356b333b1493c3c95941c95dae19a09481234e69
Hayk Hovsepyan <hhovsepy@redhat.com>
2014-01-10 10:25:07 +0100

    changed scripts to support solaris, hpux, windows

--- XTS/localjunit/crash-recovery-tests/src/main/java/com/arjuna/qa/extension/JBossAS7ServerKillProcessor.java
+++ XTS/localjunit/crash-recovery-tests/src/main/java/com/arjuna/qa/extension/JBossAS7ServerKillProcessor.java
@@ -1,119 +1,100 @@
 package com.arjuna.qa.extension;
 
-import org.jboss.arquillian.container.spi.Container;
-import org.jboss.arquillian.container.spi.ServerKillProcessor;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Map;
 import java.util.logging.Logger;
 
-public class JBossAS7ServerKillProcessor implements ServerKillProcessor {
+/**
+ * JBoss Server kill processor for Linux/Unix machines.
+ * @author <a href="mailto:hhovsepy@redhat.com">Hayk Hovsepyan</a>
+ */
+public class JBossAS7ServerKillProcessor extends BaseServerKillProcessor {
 
     private static final Logger logger = Logger.getLogger(JBossAS7ServerKillProcessor.class.getName());
-    private static final String CHECK_JBOSS_ALIVE_CMD = "if [ \"$(ps aux | grep 'jboss-module[s]')\" == \"\" ]; then exit 1; fi";
-    private static final String CHECK_FOR_DEFUNCT_JAVA_CMD = "if [ \"$(ps aux | grep '\\[java\\] <defunct>')\" == \"\" ]; then exit 1; fi";
-    private static final String SHUTDOWN_JBOSS_CMD = "ps axu | grep jboss-module[s] | awk '{print $2}' | xargs kill";
-
-    private int checkPeriodMillis = 10 * 1000;
-    private int numChecks = 60;
-
-    private static int processLogId = 0;
+    private static final String PROCESSES_CMD = ServerExtension.OSType.getOSType().getPSCommand();
+    private static final String PS_AUX_CMD = ServerExtension.isSolaris() ? "/usr/ucb/ps aux" : "ps aux";
+    private static final String CHECK_JBOSS_ALIVE_CMD = "if [ \"x`" + PROCESSES_CMD + " | grep 'jboss-module[s]'`\" = \"x\" ]; then exit 1; fi";
+    private static final String SHUTDOWN_JBOSS_CMD = PROCESSES_CMD + " | grep jboss-module[s] | awk '" + ServerExtension.OSType.getOSType().getPSIDIndex() + "' | xargs kill";
+    private static final String CHECK_FOR_DEFUNCT_JAVA_CMD = "if [ \"x`" + PS_AUX_CMD + " | grep '\\[java\\] <defunct>'`\" = \"x\" ]; then exit 1; fi";  
+    
+    @Override
+    protected boolean jbossIsAlive() throws Exception {
+        //Command will 'exit 1' if jboss is not running and 'exit 0' if it is.
+    	return runShellCommandExitCode(getJBossAliveCmd()) == 0;
+    }
+    
+    @Override
+    protected boolean isDefunctJavaProcess() throws Exception {
+        //Command will 'exit 1' if a defunct java process is not running and 'exit 0' if there is.
+        return runShellCommandExitCode(getDefunctJavaCmd()) == 0;
+    }
 
     @Override
-    public void kill(Container container) throws Exception {
-        logger.info("waiting for byteman to kill the server");
+    protected void shutdownJBoss() throws Exception {
+    	runShellCommand(getShutdownJBossCmd());
 
+        // wait 5 * 60 second for jboss-as shutdown complete
         for (int i = 0; i < numChecks; i++) {
 
             if (jbossIsAlive()) {
-                Thread.sleep(checkPeriodMillis);
-                logger.info("jboss-as is still alive, sleeping for a further " + checkPeriodMillis + "ms");
-            } else if (isDefunctJavaProcess()) {
-                logger.info("Found a defunct java process, sleeping for a further " + checkPeriodMillis + "ms");
-                dumpProcesses(container);
+            	Thread.sleep(5000);
             } else {
-                logger.info("jboss-as killed by byteman scirpt");
-                dumpProcesses(container);
+            	getLogger().info("jboss-as shutdown after sending shutdown command");
                 return;
             }
         }
-
-        //We've waited long enough for Byteman to kil the server and it has not yet done it.
-        // Kill the server manually and fail the test
-        shutdownJBoss();
-        throw new RuntimeException("jboss-as was not killed by Byteman, this indicates a test failure");
-    }
-
-    private boolean jbossIsAlive() throws Exception {
-        int exitCode = runShellCommand(CHECK_JBOSS_ALIVE_CMD);
-
-        //Command will 'exit 1' if jboss is not running and 'exit 0' if it is
-        return exitCode == 0;
     }
     
-    private boolean isDefunctJavaProcess() throws Exception {
-        int exitCode = runShellCommand(CHECK_FOR_DEFUNCT_JAVA_CMD);
-
-        //Command will 'exit 1' if a defunct java process is not running and 'exit 0' if there is
-        return exitCode == 0;
-    }
-
-    private void shutdownJBoss() throws Exception {
-        runShellCommand(SHUTDOWN_JBOSS_CMD);
-
-        // wait 5 * 60 second for jboss-as shutdown complete
-        for (int i = 0; i < 60; i++) {
-
-            if (jbossIsAlive()) {
-                Thread.sleep(5000);
-            } else {
-                logger.info("jboss-as shutdown after sending shutdown command");
-                return;
-            }
-        }
-    }
-
-    private int runShellCommand(String cmd) throws Exception {
-        logger.info("Executing shell command: '" + cmd + "'");
+    private int runShellCommandExitCode(String cmd) throws Exception {
+        getLogger().info("Executing shell command: '" + cmd + "'");
         ProcessBuilder pb = new ProcessBuilder("/bin/sh", "-c", cmd);
         Process p = pb.start();
-        p.waitFor();
-
+        
         dumpStream("std out", p.getInputStream());
         dumpStream("std error", p.getErrorStream());
-
+    
+        p.waitFor();
+	        
         p.destroy();
 
         return p.exitValue();
     }
 
-    private void dumpStream(String msg, InputStream is) {
-        try {
-            BufferedReader ein = new BufferedReader(new InputStreamReader(is));
-            String res = ein.readLine();
-            is.close();
-            if (res != null)
-            {
-                System.out.printf("%s %s\n", msg, res);
-            }
-        } catch (IOException e) {
-            logger.info("Exception dumping stream: " + e.getMessage());
-        }
-    }
+	@Override
+	protected String runShellCommand(String cmd) throws Exception {
+		getLogger().info("Executing shell command: '" + cmd + "'");
+        ProcessBuilder pb = new ProcessBuilder("/bin/sh", "-c", cmd);
+        Process p = pb.start();
+        String res = dumpStream("std out", p.getInputStream());
+        dumpStream("std error", p.getErrorStream());
+    
+        p.waitFor();
+	        
+        p.destroy();
 
-    public void dumpProcesses(Container container) throws Exception
-    {
-        Map<String, String> config = container.getContainerConfiguration().getContainerProperties();
-        String testClass = config.get("testClass");
-        String scriptName = config.get("scriptName");
+        return res;
+	}
+	
+	@Override
+	protected Logger getLogger() {
+		return logger;
+	}
 
-        String dir = "./target/surefire-reports/processes";
-        runShellCommand("mkdir -p " + dir);
+	@Override
+	protected String getJBossAliveCmd() {
+		return CHECK_JBOSS_ALIVE_CMD;
+	}
 
-        String logFile = dir + "/" + scriptName + ":" + testClass + "_" + processLogId++ + ".txt";
-        runShellCommand("ps aux > " + logFile);
-        logger.info("Logged current running processes to: " + logFile);
-    }
+	@Override
+	protected String getDefunctJavaCmd() {
+		return CHECK_FOR_DEFUNCT_JAVA_CMD;
+	}
+
+	@Override
+	protected String getShutdownJBossCmd() {
+		return SHUTDOWN_JBOSS_CMD;
+	}
+
+	@Override
+	protected String getProcessesCmd() {
+		return PS_AUX_CMD;
+	}
 }
