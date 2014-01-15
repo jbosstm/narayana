e161103f177557d1f56fd39777706b83a5a9d371
gytis <gytis@redhat.com>
2014-01-15 16:50:51 +0200

    JBTM-1184. Update documentation for Narayana and Wildfly changes

--- docs/xts_administration_and_development_guide/en-US/extras/example-client-context-handlers.java
+++
@@ -1,6 +0,0 @@
-MyService order = service.getPort(portName, MyService.class);
-
-BindingProvider bindingProvider = (BindingProvider) order;
-List<Handler> handlers = new ArrayList<Handler>(1);
-handlers.add(new JaxWSHeaderContextProcessor());
-bindingProvider.getBinding().setHandlerChain(handlers);
