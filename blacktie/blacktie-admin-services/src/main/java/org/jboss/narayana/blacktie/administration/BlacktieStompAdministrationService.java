f151c2565777114280aa21d828e1537683734a6e
Amos Feng <zfeng@redhat.com>
2014-02-13 01:56:29 +0800

    JBTM-2093 Update to set the managementAddress property in arquillian.xml

--- blacktie/blacktie-admin-services/src/main/java/org/jboss/narayana/blacktie/administration/BlacktieStompAdministrationService.java
+++ blacktie/blacktie-admin-services/src/main/java/org/jboss/narayana/blacktie/administration/BlacktieStompAdministrationService.java
@@ -168,7 +168,7 @@ public class BlacktieStompAdministrationService extends MDBBlacktieService imple
                 if (managementAddress.equals("0.0.0.0")) {
                     managementAddress = "localhost";
                 }
-                client = ModelControllerClient.Factory.create("remote", managementAddress, 9999, getCallbackHandler(), null, 20 * 1000);
+                client = ModelControllerClient.Factory.create("remote", managementAddress, 9999, getCallbackHandler(), null, 120 * 1000);
             }
         }
     }
