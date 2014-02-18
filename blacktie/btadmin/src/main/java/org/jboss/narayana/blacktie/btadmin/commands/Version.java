76a6ce05bdc4382bd8d9d2a25e89aa446e46bb5f
Tom Jenkinson <tom.jenkinson@redhat.com>
2014-02-18 15:44:10 +0000

    Upgrade Narayana to 5.0.1.Final-SNAPSHOT

--- blacktie/btadmin/src/main/java/org/jboss/narayana/blacktie/btadmin/commands/Version.java
+++ blacktie/btadmin/src/main/java/org/jboss/narayana/blacktie/btadmin/commands/Version.java
@@ -43,6 +43,6 @@ public class Version implements Command {
     }
 
     public void invoke(BlacktieAdministration connection, Properties configuration) {
-        log.info("JBoss BlackTie 5.1.0.Alpha1-SNAPSHOT");
+        log.info("JBoss BlackTie 5.0.1.Final-SNAPSHOT");
     }
 }
