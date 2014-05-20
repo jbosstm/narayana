ace9b7c6b91fb466a5f69e2fcf102c28c7d6c851
Mark Little <mlittle@redhat.com>
2014-05-20 16:43:39 +0100

    Added a SharedHammer to better simulate vert.x

---
+++ ArjunaCore/txoj/tests/classes/com/hp/mwtests/ts/txoj/hammer/SharedHammer.java
@@ -0,0 +1,65 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2006, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ *
+ * (C) 2005-2006,
+ * @author JBoss Inc.
+ */
+package com.hp.mwtests.ts.txoj.hammer;
+
+import com.arjuna.ats.arjuna.common.Uid;
+import com.hp.mwtests.ts.txoj.common.resources.AtomicObject;
+import com.hp.mwtests.ts.txoj.common.resources.HammerThreadedObject;
+
+public class SharedHammer
+{
+    public static void main (String[] args)
+    {
+        boolean creator = true;
+        Uid id = null;
+        
+        for (int i = 0; i < args.length; i++)
+        {
+            if ("-object".equals(args[i]))
+            {
+                creator = false;
+                
+                id = new Uid(args[i+1]);
+            }
+        }
+        
+        if (creator)
+        {
+            HammerThreadedObject.object = new AtomicObject();
+            
+            System.out.println("Object created with id: "+HammerThreadedObject.object);
+        }
+        else
+            HammerThreadedObject.object = new AtomicObject(id);
+        
+        HammerThreadedObject object1 = new HammerThreadedObject(1);
+        
+        object1.start();
+
+        try
+        {
+            object1.join();
+        }
+        catch (InterruptedException e)
+        {
+        }
+    }
+}
