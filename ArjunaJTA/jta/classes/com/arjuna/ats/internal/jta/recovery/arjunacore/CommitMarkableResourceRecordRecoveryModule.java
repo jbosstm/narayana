bece8bb1eb861ad4078d7773e32eefd75fac939c
Michael Musgrove <mmusgrov@redhat.com>
2014-06-11 10:12:16 +0100

    [JBTM-2191] CMR xid delete when connection is down

--- ArjunaJTA/jta/classes/com/arjuna/ats/internal/jta/recovery/arjunacore/CommitMarkableResourceRecordRecoveryModule.java
+++ ArjunaJTA/jta/classes/com/arjuna/ats/internal/jta/recovery/arjunacore/CommitMarkableResourceRecordRecoveryModule.java
@@ -682,6 +682,8 @@ public class CommitMarkableResourceRecordRecoveryModule implements
 					} catch (SQLException e) {
 						tsLogger.logger.warn("Could not handle the connection",
 								e);
+						// the connection is unavailable so try again later
+						break;
 					} finally {
 						if (connection != null) {
 							try {
