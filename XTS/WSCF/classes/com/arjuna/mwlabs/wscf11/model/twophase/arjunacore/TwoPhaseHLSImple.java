914faded1963596815e318d421100961df5c1952
gytis <gytis@redhat.com>
2014-06-30 17:21:15 +0200

    JBTM-2193. Set initialised to true only after initialisation is completed

--- XTS/WSCF/classes/com/arjuna/mwlabs/wscf11/model/twophase/arjunacore/TwoPhaseHLSImple.java
+++ XTS/WSCF/classes/com/arjuna/mwlabs/wscf11/model/twophase/arjunacore/TwoPhaseHLSImple.java
@@ -213,8 +213,6 @@ public class TwoPhaseHLSImple implements TwoPhaseHLS, UserCoordinatorService
         if (!initialised) {
             synchronized(this) {
                 if (!initialised) {
-                    // we  only do this once no matter what happens
-                    initialised = true;
                     try {
                         Class<?> factoryClass = ProtocolRegistry.sharedManager().getProtocolImplementation(coordinationType).getClass();
                         ContextProvider contextProvider = factoryClass.getAnnotation(ContextProvider.class);
@@ -229,6 +227,9 @@ public class TwoPhaseHLSImple implements TwoPhaseHLS, UserCoordinatorService
                         CONTEXT_IMPLE_CLASS = contextClass;
                     } catch (ProtocolNotRegisteredException pnre) {
                         throw new SystemException("No SOAPContext factory registered for Two Phase 1.1 service");
+                    } finally {
+                        // we  only do this once no matter what happens
+                        initialised = true;
                     }
                 }
             }
