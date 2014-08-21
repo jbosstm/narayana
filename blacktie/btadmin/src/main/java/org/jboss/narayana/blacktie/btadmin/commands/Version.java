a49a483c88cb85dc1bdda5f703db78e2753c2318
Tom Jenkinson <tom.jenkinson@redhat.com>
2014-08-21 16:12:55 +0100

    Updated to 5.0.4.Final-SNAPSHOT

--- blacktie/btadmin/src/main/java/org/jboss/narayana/blacktie/btadmin/commands/Version.java
+++ blacktie/btadmin/src/main/java/org/jboss/narayana/blacktie/btadmin/commands/Version.java
@@ -43,6 +43,6 @@ public class Version implements Command {
     }
 
     public void invoke(BlacktieAdministration connection, Properties configuration) {
-        log.info("JBoss BlackTie 5.0.3.Final");
+        log.info("JBoss BlackTie 5.0.4.Final-SNAPSHOT");
     }
 }
