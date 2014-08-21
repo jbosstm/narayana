5166c9a66c41df72631362d4eacfa1dc43fbdd50
Tom Jenkinson <tom.jenkinson@redhat.com>
2014-08-21 16:12:54 +0100

    Updated to 5.0.3.Final

--- blacktie/integration-tests/src/test/java/org/jboss/narayana/blacktie/administration/BlacktieStompAdministrationServiceTest.java
+++ blacktie/integration-tests/src/test/java/org/jboss/narayana/blacktie/administration/BlacktieStompAdministrationServiceTest.java
@@ -44,7 +44,7 @@ public class BlacktieStompAdministrationServiceTest extends TestCase {
      * @throws ConnectionException
      */
     public void xtest() throws ConnectionException, ConfigurationException {
-        processStompCommand("tpadvertise,foo,FOOTest,0,queue,5.0.3.Final-SNAPSHOT,", 1);
+        processStompCommand("tpadvertise,foo,FOOTest,0,queue,5.0.3.Final,", 1);
         try {
             connection.tpacall("FOOTest", null, Connection.TPNOREPLY);
         } catch (ConnectionException e) {
@@ -59,7 +59,7 @@ public class BlacktieStompAdministrationServiceTest extends TestCase {
             // EXPECTED
         }
 
-        processStompCommand("tpadvertise,foo,FOOTest,0,queue,5.0.3.Final-SNAPSHOT,", 1);
+        processStompCommand("tpadvertise,foo,FOOTest,0,queue,5.0.3.Final,", 1);
 
         try {
             connection.tpacall("FOOTest", null, Connection.TPNOREPLY);
