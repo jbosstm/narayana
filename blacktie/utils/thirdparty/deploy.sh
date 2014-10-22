660b664b56ed1fba79b9f11561608b7471aecbef
Tom Jenkinson <tom.jenkinson@redhat.com>
2014-10-22 14:14:03 +0100

    JBTM-2260 Updated with latest nexus requirements

--- blacktie/utils/thirdparty/deploy.sh
+++ blacktie/utils/thirdparty/deploy.sh
@@ -1,21 +1,13 @@
-mvn deploy:deploy-file -Durl=https://repository.jboss.org/nexus/service/local/staging/deploy/maven2/ -DrepositoryId=jboss-releases-repository -Dfile=protoc-2.4.1.3-centos55x32.zip -DpomFile=poms/protoc-2.4.1.3.pom -Dpackaging=zip -Dclassifier=centos55x32
-mvn deploy:deploy-file -Durl=https://repository.jboss.org/nexus/service/local/staging/deploy/maven2/ -DrepositoryId=jboss-releases-repository -Dfile=protoc-2.4.1.3-centos54x64.zip -DpomFile=poms/protoc-2.4.1.3.pom -Dpackaging=zip -Dclassifier=centos54x64
-mvn deploy:deploy-file -Durl=https://repository.jboss.org/nexus/service/local/staging/deploy/maven2/ -DrepositoryId=jboss-releases-repository -Dfile=protoc-2.4.1.3-vc9x32.zip -DpomFile=poms/protoc-2.4.1.3.pom -Dpackaging=zip -Dclassifier=vc9x32
+export bpa=centos70x64
 
-#for i in apr-1-1.3.3 log4cxx-902683
-#do
-#mvn deploy:deploy-file -Durl=https://repository.jboss.org/nexus/service/local/staging/deploy/maven2/ -DrepositoryId=jboss-releases -Dfile=`pwd`/build/lib/$i-$bpa.jar -DpomFile=`pwd`/poms/$i.pom -Dpackaging=jar -#Dclassifier=$bpa
-#done
+# This will fail as it conflicts with some nexus rules for jar deployment, you can't replace the artifact but you can't release a version of the jar with just a classifier and not the main jar unless it is a zip
+for i in apr-1-1.3.3 log4cxx-902683
+do
+mvn deploy:deploy-file -Durl=https://repository.jboss.org/nexus/service/local/staging/deploy/maven2/ -DrepositoryId=jboss-releases-repository -Dfile=`pwd`/$bpa/$i-$bpa.jar -DpomFile=`pwd`/poms/$i.pom -Dpackaging=jar -Dclassifier=$bpa -Dsources=`pwd`/$bpa/$i-$bpa.jar
+mvn deploy:deploy-file -Durl=https://repository.jboss.org/nexus/service/local/staging/deploy/maven2/ -DrepositoryId=jboss-releases-repository -Dfile=`pwd`/$bpa/$i-$bpa.jar -DpomFile=`pwd`/poms/$i.pom -Dpackaging=jar
+done
 
-#export bpa=centos55x32
-
-#for i in ace-5.7.6 cppunit-1.12 expat-2.0.1 xercesc-3.0.1
-#do
-#mvn deploy:deploy-file -Durl=https://repository.jboss.org/nexus/service/local/staging/deploy/maven2/ -DrepositoryId=jboss-releases -Dfile=`pwd`/build/lib/$i-$bpa.zip -DpomFile=`pwd`/poms/$i.pom -Dpackaging=zip -Dclassifier=$bpa
-#done
-
-
-#for i in apr-1-1.3.3 log4cxx-902683
-#do
-#mvn deploy:deploy-file -Durl=https://repository.jboss.org/nexus/service/local/staging/deploy/maven2/ -DrepositoryId=jboss-releases -Dfile=`pwd`/build/lib/$i-$bpa.jar -DpomFile=`pwd`/poms/$i.pom -Dpackaging=jar -Dclassifier=$bpa
-#done
+for i in protoc-2.4.1.3 cppunit-1.12 expat-2.0.1 xercesc-3.0.1
+do
+mvn deploy:deploy-file -Durl=https://repository.jboss.org/nexus/service/local/staging/deploy/maven2/ -DrepositoryId=jboss-releases-repository -Dfile=`pwd`/$bpa/$i-$bpa.zip -DpomFile=`pwd`/poms/$i.pom -Dpackaging=zip -Dclassifier=$bpa -Dsources=`pwd`/$bpa/$i-$bpa.zip
+done
