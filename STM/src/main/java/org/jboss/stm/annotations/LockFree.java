f4c406654f54e37081edfd59b5b521d92ed13df2
Mark Little <mlittle@redhat.com>
2014-12-02 20:20:19 +0000

    Locks need to be inherited for safety/consistency
    https://issues.jboss.org/browse/JBTM-2173

--- STM/src/main/java/org/jboss/stm/annotations/LockFree.java
+++ STM/src/main/java/org/jboss/stm/annotations/LockFree.java
@@ -32,6 +32,7 @@ import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.METHOD })
+@Inherited
 public @interface LockFree
 {
 }
--- STM/src/main/java/org/jboss/stm/annotations/ReadLock.java
+++ STM/src/main/java/org/jboss/stm/annotations/ReadLock.java
@@ -31,6 +31,7 @@ import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.METHOD })
+@Inherited
 public @interface ReadLock
 {
 }
