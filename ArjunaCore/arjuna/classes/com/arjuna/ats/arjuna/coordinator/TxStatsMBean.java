d57cfa9106cf04a7fcbfdda3ccabcdd61ea28b23
Michael Musgrove <mmusgrov@redhat.com>
2015-02-23 10:16:35 +0000

    [JBTM-2341] More explanation of the meaning of the averageCommit time statistic

--- ArjunaCore/arjuna/classes/com/arjuna/ats/arjuna/coordinator/TxStatsMBean.java
+++ ArjunaCore/arjuna/classes/com/arjuna/ats/arjuna/coordinator/TxStatsMBean.java
@@ -53,8 +53,20 @@ public interface TxStatsMBean
     long getNumberOfCommittedTransactions();
 
     /**
-     * Returns the average time it is taking to commit a transaction.
-     * Note that a small number of stuck transactions can skew the overall average.
+     * Returns the average time, in nanoseconds, it is taking to commit a transaction. This time is
+     * measured from the moment the client calls commit until the transaction manager determines that the
+     * commit attempt was successful (ie that all participants successfully committed). This includes cases where:
+     *
+     * <ul>
+     *   <li>there are no transaction participants;
+     *   <li>the transaction only contains readonly participants;
+     * </ul>
+     *
+     * The average will not be updated if any participants failed to commit.
+     *
+     * Note that a small number of stuck transactions can skew the overall average. Similarly the average time
+     * will be reduced if there are many transactions without participants or with only readonly participants.
+     *
      * @return the average time, in nanoseconds, it has taken to commit a transaction.
      */
     long getAverageCommitTime();
@@ -90,8 +102,8 @@ public interface TxStatsMBean
      * Returns the number of transactions that have been rolled back due to internal system errors including
      * failure to create log storage and failure to write a transaction log. It does not include rollbacks
      * caused by resource failures.
-	 * @return the number of transactions that been rolled back due to internal system errors
-	 */
+     * @return the number of transactions that been rolled back due to internal system errors
+     */
     long getNumberOfSystemRollbacks();
 
     /**
