648d3f144ca54a3785cb40f1560b0afd5e49359f
Michael Musgrove <mmusgrov@redhat.com>
2015-03-31 19:36:09 +0100

    JBTM-2347 Use regexs to choose benchmarks

--- scripts/hudson/benchmark.sh
+++ scripts/hudson/benchmark.sh
@@ -1,7 +1,8 @@
 
 # -i (1 iteration), -wi (10 warm ups), -r (300 seconds at each iteration)
 # use java -jar <maven module>/target/benchmarks.jar -h for options
-[ -z "${JMHARGS}" ] && JMHARGS="-i 1 -wi 10 -f 1 -r 300 -rf csv -rff"
+#[ -z "${JMHARGS}" ] && JMHARGS="-i 1 -wi 10 -f 1 -r 300"
+[ -z "${JMHARGS}" ] && JMHARGS="-i 1 -wi 1 -f 1 -r 1"
 
 [ -z "${WORKSPACE}" ] && WORKSPACE=`pwd`
 MAVEN_HOME=$WORKSPACE/tools/maven
@@ -10,11 +11,13 @@ ofile=$WORKSPACE/benchmark-output.txt
 
 # run a set of benchmarks and copy the generated jmh csv files to $WORKSPACE
 function run_bm {
-  CSV_DIR="$1/target/jmh"
+  suffix=".\*"
+  f=${2%$suffix}
+  CSV_DIR="$f/target/jmh"
   [ -d $CSV_DIR ] || mkdir -p $CSV_DIR
+  CSVF="$CSV_DIR/$f-$3.csv"
 
-  CSVF="$CSV_DIR/$2-$3.csv"
-  java -cp $1/target/benchmarks.jar $2 $JMHARGS  -rf csv -rff $CSVF
+  java -jar $1/target/benchmarks.jar "$2" $JMHARGS -rf csv -rff $CSVF
 
   cp $CSVF $WORKSPACE # the jmh plugin looks for csv files in $WORKSPACE
 }
@@ -40,26 +43,36 @@ function build_benchmark {
   git clone https://github.com/jbosstm/performance
   cd performance/narayana
 
-  mvn package # build the benchmarks
+  mvn package -DskipTests # build the benchmarks
 }
 
 BMDIR=$WORKSPACE/tmp/performance/narayana
-BM1=com.hp.mwtests.ts.arjuna.performance.Performance1
-BM2=com.hp.mwtests.ts.arjuna.atomicaction.CheckedActionTest
-BM3=com.arjuna.ats.jta.xa.performance.JTAStoreTests
+BM1="com.hp.mwtests.ts.arjuna.performance.Performance1.*"
+BM2="com.hp.mwtests.ts.arjuna.atomicaction.CheckedActionTest.*"
+BM3="com.arjuna.ats.jta.xa.performance.JTAStoreTests.*"
+BM4="io.narayana.perf.product.ProductComparison.*"
 
 function run_benchmarks {
   cd $WORKSPACE/tmp/performance/narayana
-  run_bm $BMDIR/ArjunaCore/arjuna $BM1 $1
-  run_bm $BMDIR/ArjunaJTA/jta $BM3 $1
+  run_bm $BMDIR/ArjunaCore/arjuna "$BM1" $1
+  run_bm $BMDIR/ArjunaCore/arjuna "$BM2" $1
+  run_bm $BMDIR/ArjunaJTA/jta "$BM3" $1
 }
 
 function regression_check {
   cd $WORKSPACE/tmp/performance/narayana
-# "$@" should only contain jvm args (such as -D -X etc)
-  java "$@" -cp tools/target/classes io.narayana.perf.jmh.Benchmark $WORKSPACE/*.csv >> $ofile
+  ls $WORKSPACE/*.csv > /dev/null 2>&1
+  res=$?
 
-  return $?
+  if [ $res = 0 ]; then
+    # "$@" should only contain jvm args (such as -D -X etc)
+    java "$@" -cp tools/target/classes io.narayana.perf.jmh.Benchmark $WORKSPACE/*.csv >> $ofile
+    res=$?
+  else
+    echo "no benchmarks to compare"
+  fi
+
+  return $res
 }
 
 function generate_csv_files {
@@ -74,6 +87,6 @@ echo "JMH benchmark run (with args $JMHARGS)\n" > $ofile
 generate_csv_files
 regression_check "$@"
 rv=$?
-#rm $WORKSPACE/*.csv
 cd $WORKSPACE
+
 exit $rv
