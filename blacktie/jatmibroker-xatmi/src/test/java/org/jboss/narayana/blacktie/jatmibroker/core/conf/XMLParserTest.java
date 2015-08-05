4048f04173a8f7a4778817e2d2dee9817f263b99
Tom Jenkinson <tom.jenkinson@redhat.com>
2015-08-05 10:40:36 +0100

    Updated to 5.2.1.Final

--- blacktie/jatmibroker-xatmi/src/test/java/org/jboss/narayana/blacktie/jatmibroker/core/conf/XMLParserTest.java
+++ blacktie/jatmibroker-xatmi/src/test/java/org/jboss/narayana/blacktie/jatmibroker/core/conf/XMLParserTest.java
@@ -123,7 +123,7 @@ public class XMLParserTest extends TestCase {
         String function = "org.jboss.narayana.blacktie.jatmibroker.xatmi.services.TPCallXOctetService";
 
         String userlist = "guest:true:true,blacktie:true:true";
-        String version = "5.2.1.Final-SNAPSHOT";
+        String version = "5.2.1.Final";
 
         assertTrue(server.equals(prop.getProperty("blacktie.TestOne.server")));
         assertTrue(function.equals(prop.getProperty("blacktie.JAVA_Converse.java_class_name")));
