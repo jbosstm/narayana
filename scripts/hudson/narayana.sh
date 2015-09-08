67a4bb940fd376cc9a8c0b77caec5b9282ab99e3
Michael Musgrove <mmusgrov@redhat.com>
2015-09-08 17:00:45 +0100

    JBTM-2508 configure JDK logging

--- scripts/hudson/narayana.sh
+++ scripts/hudson/narayana.sh
@@ -419,7 +419,8 @@ function tx_bridge_tests {
 }
 
 function enable_qa_trace {
-cat << 'EOF' > $WORKSPACE/qa/dist/${NARAYANA_VERSION}/etc/log4j.xml
+echo "creating file $WORKSPACE/qa/dist/narayana-full-${NARAYANA_CURRENT_VERSION}/etc/log4j.xml"
+cat << 'EOF' > $WORKSPACE/qa/dist/narayana-full-${NARAYANA_CURRENT_VERSION}/etc/log4j.xml
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE log4j:configuration SYSTEM "log4j.dtd">
 
@@ -451,6 +452,13 @@ cat << 'EOF' > $WORKSPACE/qa/dist/${NARAYANA_VERSION}/etc/log4j.xml
     </category>
 </log4j:configuration>
 EOF
+
+cat << 'EOF2' > $WORKSPACE/qa/dist/narayana-full-${NARAYANA_CURRENT_VERSION}/etc/logging.properties
+handlers= java.util.logging.ConsoleHandler
+.level= ALL
+java.util.logging.ConsoleHandler.level = ALL
+java.util.logging.ConsoleHandler.formatter = java.util.logging.SimpleFormatter
+EOF2
 }
 
 function enable_xts_trace {
@@ -517,6 +525,10 @@ function qa_tests_once {
     openjdkjar="dist/narayana-full-${NARAYANA_CURRENT_VERSION}/lib/ext/openjdk-orb.jar"
     EXTRA_QA_SYSTEM_PROPERTIES="-Xbootclasspath/p:$openjdkjar $EXTRA_QA_SYSTEM_PROPERTIES"
     orbtype="idlj"
+  fi
+
+  if [ $QA_TRACE ]; then
+    EXTRA_QA_SYSTEM_PROPERTIES="-Djava.util.logging.config.file=dist/narayana-full-${NARAYANA_CURRENT_VERSION}/etc/logging.properties $EXTRA_QA_SYSTEM_PROPERTIES"
   fi
 
   if [[ x"$EXTRA_QA_SYSTEM_PROPERTIES" != "x" ]]; then
