0ae702217f35132d63aed2aafe5c9a3ac482b47f
Tom Jenkinson <tom.jenkinson@redhat.com>
2015-09-22 22:49:52 +0100

    Updated to 5.2.5.Final-SNAPSHOT

--- ArjunaCore/arjuna/classes/com/arjuna/ats/arjuna/coordinator/HeuristicInformation.java
+++ ArjunaCore/arjuna/classes/com/arjuna/ats/arjuna/coordinator/HeuristicInformation.java
@@ -39,7 +39,7 @@ package com.arjuna.ats.arjuna.coordinator;
  * @author Richard A. Begg (richard.begg@arjuna.com)
  */
 /**
- * @deprecated as of 5.2.4.Final In a subsequent release we will be providing this functionality via JMX MBeans
+ * @deprecated as of 5.2.5.Final-SNAPSHOT In a subsequent release we will be providing this functionality via JMX MBeans
  */
 @Deprecated
 public interface HeuristicInformation
--- blacktie/btadmin/src/main/java/org/jboss/narayana/blacktie/btadmin/commands/Version.java
+++ blacktie/btadmin/src/main/java/org/jboss/narayana/blacktie/btadmin/commands/Version.java
@@ -43,6 +43,6 @@ public class Version implements Command {
     }
 
     public void invoke(BlacktieAdministration connection, Properties configuration) {
-        log.info("JBoss BlackTie 5.2.4.Final");
+        log.info("JBoss BlackTie 5.2.5.Final-SNAPSHOT");
     }
 }
