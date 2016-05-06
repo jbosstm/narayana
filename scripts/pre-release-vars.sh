6baf9044ffee141c396447160ea50cb32a2ae0d2
Gytis Trikleris <gytis@redhat.com>
2016-05-06 14:25:49 +0200

    Updated to 4.17.33.Final

--- scripts/pre-release-vars.sh
+++ scripts/pre-release-vars.sh
@@ -1,4 +1,4 @@
 export BRANCH=4.17
 #export CURRENT_SNAPSHOT_VERSION=`cat ../pom.xml | grep "<version>" | head -1 | sed "s/.*<version>//" | sed "s/<\/version>//"`
-export CURRENT_SNAPSHOT_VERSION=4.17.33.Final-SNAPSHOT
+export CURRENT_SNAPSHOT_VERSION=4.17.33.Final
 
