9d12384676a8fe551ab6c240809f39c6968648f0
Tom Jenkinson <tom.jenkinson@redhat.com>
2016-05-16 14:30:57 +0100

    rebase on 5.2

--- scripts/hudson/narayana-rebase.bat
+++ scripts/hudson/narayana-rebase.bat
@@ -1,7 +1,3 @@
 git rebase --abort
 git clean -f -d -x
-git branch -D 4.17
-git branch 4.17 origin/4.17
-git branch -D master
-git branch master origin/master
-git pull --rebase --ff-only origin master
+git pull --rebase --ff-only origin 5.2
