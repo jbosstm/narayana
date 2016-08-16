603833f23c12f1dc358f38966eb35f2723efb012
Tom Jenkinson <tom.jenkinson@redhat.com>
2016-08-16 14:52:29 +0100

    Updated nexus upload script for BlackTie thirdparty libraries

--- blacktie/utils/thirdparty/deploy.sh
+++ blacktie/utils/thirdparty/deploy.sh
@@ -1,13 +1,22 @@
-export bpa=centos70x64
-
 # This will fail as it conflicts with some nexus rules for jar deployment, you can't replace the artifact but you can't release a version of the jar with just a classifier and not the main jar unless it is a zip
-for i in apr-1-1.3.3 log4cxx-902683
-do
-mvn deploy:deploy-file -Durl=https://repository.jboss.org/nexus/service/local/staging/deploy/maven2/ -DrepositoryId=jboss-releases-repository -Dfile=`pwd`/$bpa/$i-$bpa.jar -DpomFile=`pwd`/poms/$i.pom -Dpackaging=jar -Dclassifier=$bpa -Dsources=`pwd`/$bpa/$i-$bpa.jar
-mvn deploy:deploy-file -Durl=https://repository.jboss.org/nexus/service/local/staging/deploy/maven2/ -DrepositoryId=jboss-releases-repository -Dfile=`pwd`/$bpa/$i-$bpa.jar -DpomFile=`pwd`/poms/$i.pom -Dpackaging=jar
-done
 
-for i in protoc-2.4.1.3 cppunit-1.12 expat-2.0.1 xercesc-3.0.1
-do
-mvn deploy:deploy-file -Durl=https://repository.jboss.org/nexus/service/local/staging/deploy/maven2/ -DrepositoryId=jboss-releases-repository -Dfile=`pwd`/$bpa/$i-$bpa.zip -DpomFile=`pwd`/poms/$i.pom -Dpackaging=zip -Dclassifier=$bpa -Dsources=`pwd`/$bpa/$i-$bpa.zip
+echo "You MUST have all files ready for deploy from all BPA in order to run this script or subsequent runs will fail with artifact uniqueness constraints - if you agree enter Y"
+read y
+if [ $y != Y ]
+then
+  echo "OK - try when you have them"
+  exit
+fi
+
+for i in vc9x32 centos54x64 centos55x32 fc18x64
+  for j in apr-1-1.5.2.BT2 log4cxx-902683
+  do
+    mvn deploy:deploy-file -Durl=https://repository.jboss.org/nexus/service/local/staging/deploy/maven2/ -DrepositoryId=jboss-releases-repository -Dfile=$j-$i.jar -DpomFile=$j.pom -Dpackaging=jar -Dclassifier=$i
+  done
+  mvn deploy:deploy-file -Durl=https://repository.jboss.org/nexus/service/local/staging/deploy/maven2/ -DrepositoryId=jboss-releases-repository -Dfile=$j-vc9x32.jar -DpomFile=$j.pom -Dpackaging=jar -Dsources=$j-sources.jar
+
+  for j in protoc-2.4.1.3 cppunit-1.12 expat-2.0.1 xercesc-3.0.1
+  do
+    mvn deploy:deploy-file -Durl=https://repository.jboss.org/nexus/service/local/staging/deploy/maven2/ -DrepositoryId=jboss-releases-repository -Dfile=$j-$i.zip -DpomFile=$j.pom -Dpackaging=zip -Dclassifier=$i -Dsources=$j-sources.zip
+  done
 done
