ce04d5051f33b9a496506fa6754fd4d4cbb4d0fb
Tom Jenkinson <tom.jenkinson@redhat.com>
2016-09-06 11:00:19 +0100

    Added a note to the CI script to mention the name of the branch that is being tested

--- ArjunaJTS/jtax/tests/classes/com/arjuna/ats/internal/jta/recovery/jts/JCAServerTransactionRecoveryModuleTest.java
+++ ArjunaJTS/jtax/tests/classes/com/arjuna/ats/internal/jta/recovery/jts/JCAServerTransactionRecoveryModuleTest.java
@@ -72,7 +72,7 @@ public class JCAServerTransactionRecoveryModuleTest {
     }
 
     @Test
-    public void testIdempotentLoad() throws XAException, RollbackException, SystemException {
+    public void testReloadStateAcceptable() throws XAException, RollbackException, SystemException {
         JCAServerTransactionRecoveryModule module = new JCAServerTransactionRecoveryModule();
         XATerminator terminator = new XATerminatorImple();
         Xid xid = new Xid() {
@@ -147,7 +147,9 @@ public class JCAServerTransactionRecoveryModuleTest {
         });
         terminator.prepare(xid);
         module.periodicWorkFirstPass();
+        module.periodicWorkSecondPass();
         module.periodicWorkFirstPass();
+        module.periodicWorkSecondPass();
         assertFalse(commitCalled);
         terminator.commit(xid, false);
         assertTrue(commitCalled);
