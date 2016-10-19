774e30871aaa276d9389ed6cc624062a99f2e692
Tom Jenkinson <tom.jenkinson@redhat.com>
2016-10-19 20:40:43 +0100

    Make sure that the note to the CI script to mention the name of the branch that is being tested is only printed when a non-default branch is requested

--- scripts/hudson/narayana.sh
+++ scripts/hudson/narayana.sh
@@ -284,7 +284,7 @@ function build_as {
   [ $? = 0 ] || fatal "git fetch of pulls failed"
   [ -z "$AS_BRANCH" ] || git checkout $AS_BRANCH
   [ $? = 0 ] || fatal "git fetch of pull branch failed"
-  comment_on_pull "$BUILD_URL using non-default AS_BRANCH: $AS_BRANCH"
+  [ -z "$AS_BRANCH" ] || comment_on_pull "$BUILD_URL using non-default AS_BRANCH: $AS_BRANCH"
 
   git fetch upstream
   echo "This is the JBoss-AS commit"
