68f2427aa79758502a458070e30da6731ad4d32a
Tom Jenkinson <tom.jenkinson@redhat.com>
2016-10-20 09:59:01 +0100

    Updated to 4.17.38.Final-SNAPSHOT

--- scripts/pre-release-vars.sh
+++ scripts/pre-release-vars.sh
@@ -1,4 +1,4 @@
 export BRANCH=4.17
 #export CURRENT_SNAPSHOT_VERSION=`cat ../pom.xml | grep "<version>" | head -1 | sed "s/.*<version>//" | sed "s/<\/version>//"`
-export CURRENT_SNAPSHOT_VERSION=4.17.37.Final
+export CURRENT_SNAPSHOT_VERSION=4.17.38.Final-SNAPSHOT
 
