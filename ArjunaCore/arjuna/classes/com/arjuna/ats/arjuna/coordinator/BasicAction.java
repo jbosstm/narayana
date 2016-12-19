4e0e21fdd1e5de955d9b1caee3a32f3432ad0ad7
Tom Jenkinson <tom.jenkinson@redhat.com>
2016-12-19 13:36:22 +0000

    JBTM-2822 Added support for suppressed exceptions in prepare

--- ArjunaCore/arjuna/classes/com/arjuna/ats/arjuna/coordinator/BasicAction.java
+++ ArjunaCore/arjuna/classes/com/arjuna/ats/arjuna/coordinator/BasicAction.java
@@ -2728,6 +2728,7 @@ public class BasicAction extends StateManager
                               * Prepare on this record failed - we are in trouble.
                               * Add the record back onto the pendingList and return.
                               */
+                    addDeferredThrowables(record, deferredThrowables);
 
                     record = insertRecord(pendingList, record);
 
