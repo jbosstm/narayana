77a1433bac607d74cca13059763516a4475d5353
Tom Jenkinson <tom.jenkinson@redhat.com>
2017-01-18 09:30:29 +0000

    Updated to 5.2.23.Final-SNAPSHOT

--- blacktie/btadmin/src/main/java/org/jboss/narayana/blacktie/btadmin/commands/Version.java
+++ blacktie/btadmin/src/main/java/org/jboss/narayana/blacktie/btadmin/commands/Version.java
@@ -43,6 +43,6 @@ public class Version implements Command {
     }
 
     public void invoke(BlacktieAdministration connection, Properties configuration) {
-        log.info("JBoss BlackTie 5.2.22.Final");
+        log.info("JBoss BlackTie 5.2.23.Final-SNAPSHOT");
     }
 }
