327a4aab69eee80b2b74a863c8ad33b956e38a84
Tom Jenkinson <tom.jenkinson@redhat.com>
2017-03-31 18:40:38 +0100

    BUILD SCRIPT ONLY: No need to update website

--- narayana-release-process.sh
+++ narayana-release-process.sh
@@ -52,9 +52,6 @@ then
   git add pom.xml
   git commit -m "${WFLYISSUE} Upgrade Narayana to $CURRENT"
   git push --set-upstream jbosstm ${WFLYISSUE}
-  git checkout 5_BRANCH
-  git reset --hard jbosstm/5_BRANCH
-  cd -
 fi
 set +e
 git fetch upstream --tags
@@ -75,18 +72,3 @@ mvn clean -gs tools/maven/conf/settings.xml -Dorson.jar.location=./ext/
 mvn clean deploy -DskipTests -gs tools/maven/conf/settings.xml -Dorson.jar.location=./ext/ -Prelease
 mvn clean deploy -DskipTests -gs tools/maven/conf/settings.xml -Prelease -f blacktie/utils/cpp-plugin/pom.xml
 mvn clean deploy -DskipTests -gs tools/maven/conf/settings.xml -Prelease  -f blacktie/pom.xml -pl :blacktie-jatmibroker-nbf -am
-git archive -o ../../narayana-full-$CURRENT-src.zip $CURRENT
-cd -
-cd ~/tmp/narayana/$CURRENT/sources/documentation/
-git checkout $CURRENT
-mvn clean install -Prelease
-cd -
-ant -f build-release-pkgs.xml -Dawestruct.executable="awestruct" all
-
-echo "build and retrieve the centos54x64 and vc9x32 binaries from http://narayanaci1.eng.hst.ams2.redhat.com/view/Release/"
-echo "Press enter when the artifacts are available"
-read
-wget http://narayanaci1.eng.hst.ams2.redhat.com/view/Release/job/release-narayana/lastSuccessfulBuild/artifact/blacktie/blacktie/target/blacktie-${CURRENT}-centos54x64-bin.tar.gz
-wget http://narayanaci1.eng.hst.ams2.redhat.com/view/Release/job/release-narayana-catelyn/lastSuccessfulBuild/artifact/blacktie/blacktie/target/blacktie-${CURRENT}-vc9x32-bin.zip
-scp blacktie-${CURRENT}-centos54x64-bin.tar.gz jbosstm@filemgmt.jboss.org:/downloads_htdocs/jbosstm/${CURRENT}/binary/
-scp blacktie-${CURRENT}-vc9x32-bin.zip jbosstm@filemgmt.jboss.org:/downloads_htdocs/jbosstm/${CURRENT}/binary/
