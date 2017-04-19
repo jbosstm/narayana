c851031ac6232b9a8647922e7a71ef8dd1403b38
Tom Jenkinson <tom.jenkinson@redhat.com>
2017-04-19 12:19:19 +0100

    BUILD SCRIPT ONLY: updated to clarify the required permissions

--- narayana-release-process.sh
+++ narayana-release-process.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-if [[ $(uname) == CYGWIN* ]]
-then
-  read -p "ARE YOU RUNNING AN ELEVATED CMD PROMPT mvn.cmd needs this" ELEV
-  if [[ $ELEV == n* ]]
-  then
-    exit
-  fi
-fi
-read -p "You will need: VPN, credentials for jbosstm@filemgmt, jira admin, github permissions on all jbosstm/ repo. Do you have these?" ENVOK
+read -p "You will need: jira admin, github permissions on all jbosstm/ repo and nexus permissions. Do you have these?" ENVOK
 if [[ $ENVOK == n* ]]
 then
   exit
@@ -19,56 +11,45 @@ if [ $# -eq 0 ]; then
   NEXT=`echo $CURRENT_SNAPSHOT_VERSION | sed "s/.Final//"`
   NEXT="${NEXT%.*}.$((${NEXT##*.}+1))".Final
 elif [ $# -lt 2 ]; then
-  echo 1>&2 "$0: not enough arguments: CURRENT NEXT <WFLYISSUE>(versions should end in .Final or similar)"
+  echo 1>&2 "$0: not enough arguments: CURRENT NEXT (versions should end in .Final or similar)"
   exit 2
-elif [ $# -gt 3 ]; then
-  echo 1>&2 "$0: too many arguments: CURRENT NEXT <WFLYISSUE>(versions should end in .Final or similar)"
+elif [ $# -gt 2 ]; then
+  echo 1>&2 "$0: too many arguments: CURRENT NEXT (versions should end in .Final or similar)"
   exit 2
 else
   CURRENT=$1
   NEXT=$2
-  WFLYISSUE=$3
 fi
 
-set -e
-if [ -z "$WFLYISSUE" ]
-then
-  JENKINS_JOBS=narayana,narayana-catelyn,narayana-codeCoverage,narayana-documentation,narayana-hqstore,narayana-ibm-jdk,narayana-jdbcobjectstore,narayana-quickstarts,narayana-quickstarts-catelyn ./scripts/release/pre_release.py
-  ./scripts/release/update_jira.py -k JBTM -t 5.next -n $CURRENT
-  read -p "Enter WFLY issue: " WFLYISSUE
-  if [ ! -d "jboss-as" ]
-  then
-    (git clone git@github.com:jbosstm/jboss-as.git -o jbosstm; cd jboss-as; git remote add upstream-wildfly git@github.com:wildfly/wildfly.git)
-  fi
-  (cd jboss-as; git fetch upstream-wildfly; git checkout -b ${WFLYISSUE}; git reset --hard upstream-wildfly/master)
-  cd jboss-as
-  CURRENT_VERSION_IN_WFLY=`grep 'narayana>' pom.xml | cut -d \< -f 2|cut -d \> -f 2`
-  if [[ $(uname) == CYGWIN* ]]
-  then
-    sed -i "s/narayana>$CURRENT_VERSION_IN_WFLY/narayana>$CURRENT/g" pom.xml
-  else
-    sed -i "" "s/narayana>$CURRENT_VERSION_IN_WFLY/narayana>$CURRENT/g" pom.xml
-  fi
-  git add pom.xml
-  git commit -m "${WFLYISSUE} Upgrade Narayana to $CURRENT"
-  git push --set-upstream jbosstm ${WFLYISSUE}
-fi
 set +e
 git fetch upstream --tags
 git tag | grep $CURRENT
 if [[ $? != 0 ]]
 then
   set -e
+  git checkout 5.5
+  git log -n 10
+  echo Mark version as released in Jira and create next version: https://issues.jboss.org/plugins/servlet/project-config/JBTM/versions
+  echo Make sure you have the credentials in your .m2/settings.xml and ignore an error in the final module about missing javadocs
+  echo Watch out for sed -i "" in the pre-release.sh as it is does not work on Cygwin
+  read -p "Did the log before look OK?" ok
+  if [[ $ok == n* ]]
+  then
+    exit
+  else
+    ok=y
+  fi
   (cd ./scripts/ ; ./pre-release.sh $CURRENT $NEXT)
-  git fetch upstream --tags
 else
   set -e
+  ok=y
 fi
 
-git checkout $CURRENT; MAVEN_OPTS="-XX:MaxPermSize=512m" 
-cd ~/tmp/narayana/$CURRENT/sources/narayana/
-git checkout $CURRENT
-mvn clean -gs tools/maven/conf/settings.xml -Dorson.jar.location=./ext/
-mvn clean deploy -DskipTests -gs tools/maven/conf/settings.xml -Dorson.jar.location=./ext/ -Prelease
-mvn clean deploy -DskipTests -gs tools/maven/conf/settings.xml -Prelease -f blacktie/utils/cpp-plugin/pom.xml
-mvn clean deploy -DskipTests -gs tools/maven/conf/settings.xml -Prelease  -f blacktie/pom.xml -pl :blacktie-jatmibroker-nbf -am
+if [[ $ok == y* ]]
+then
+  git fetch upstream
+  git checkout $CURRENT
+  git clean -f -d
+  # Add -x (this will delete all files (e.g. IDE, new features) not under source control)
+  mvn clean deploy -Prelease -Dmaven.javadoc.skip=true -DskipTests -pl :narayana-jts-idlj,:jbossxts,:narayana-jts-integration,:byteman_support,:jbosstxbridge -am
+fi
