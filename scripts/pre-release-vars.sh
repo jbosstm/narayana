9c1c70ab9784b619489607f54040a31410422749
Ondra Chaloupka <ochaloup@redhat.com>
2017-04-19 14:38:01 +0200

    Updated to 4.17.41.Final-SNAPSHOT

--- scripts/pre-release-vars.sh
+++ scripts/pre-release-vars.sh
@@ -1,4 +1,4 @@
 export BRANCH=4.17
 #export CURRENT_SNAPSHOT_VERSION=`cat ../pom.xml | grep "<version>" | head -1 | sed "s/.*<version>//" | sed "s/<\/version>//"`
-export CURRENT_SNAPSHOT_VERSION=4.17.40.Final
+export CURRENT_SNAPSHOT_VERSION=4.17.41.Final-SNAPSHOT
 
