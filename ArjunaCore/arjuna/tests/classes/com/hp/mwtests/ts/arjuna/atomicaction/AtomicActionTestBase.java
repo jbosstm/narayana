74e6afabaa851d3976bc11a6308b11cba084543c
Tom Jenkinson <tom.jenkinson@redhat.com>
2017-04-27 15:12:27 +0100

    TEST ONLY: testAbort and testCommit had same behaviour

--- ArjunaCore/arjuna/tests/classes/com/hp/mwtests/ts/arjuna/atomicaction/AtomicActionTestBase.java
+++ ArjunaCore/arjuna/tests/classes/com/hp/mwtests/ts/arjuna/atomicaction/AtomicActionTestBase.java
@@ -50,7 +50,7 @@ public class AtomicActionTestBase
 
     protected void testAbort () throws Exception
     {
-        executeTest(true, ActionStatus.COMMITTED, null, new BasicRecord(), new BasicRecord());
+        executeTest(false, ActionStatus.ABORTED, null, new BasicRecord(), new BasicRecord());
     }
 
     protected void testPrepareWithLRRSuccess()
