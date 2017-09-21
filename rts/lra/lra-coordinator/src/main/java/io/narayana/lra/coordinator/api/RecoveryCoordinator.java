52c89eaef6f356f4be3d7a2e9ff56745d6d616d3
Michael Musgrove <mmusgrov@redhat.com>
2017-09-21 23:22:31 +0100

    JBTM-2930 Fixed a number of recovery problems

--- rts/lra/lra-coordinator/src/main/java/io/narayana/lra/coordinator/api/RecoveryCoordinator.java
+++ rts/lra/lra-coordinator/src/main/java/io/narayana/lra/coordinator/api/RecoveryCoordinator.java
@@ -21,6 +21,7 @@
  */
 package io.narayana.lra.coordinator.api;
 
+import io.narayana.lra.client.GenericLRAException;
 import io.narayana.lra.coordinator.domain.service.LRAService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -34,12 +35,16 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
-import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @ApplicationScoped
 @Path(LRAClient.RECOVERY_COORDINATOR_PATH_NAME)
@@ -48,12 +53,15 @@ public class RecoveryCoordinator {
 
     private final Logger logger = Logger.getLogger(RecoveryCoordinator.class.getName());
 
+    @Context
+    private UriInfo context;
+
     @Inject
     private LRAService lraService;
 
     // Performing a GET on the recovery URL (return from a join request) will return the original <compensator URL>
     @GET
-    @Path("{RecCoordId}")
+    @Path("{LRAId}/{RecCoordId}")
     @Produces(MediaType.APPLICATION_JSON)
     @ApiOperation(value = "Lookup the compensator URL",
             notes = "Performing a GET on the recovery URL (returned from a join request) will return the original compensator URL(s)",
@@ -63,6 +71,8 @@ public class RecoveryCoordinator {
             @ApiResponse( code = 200, message = "The compensator associated with this recovery id is returned" )
     } )
     public String getCompensator(
+            @ApiParam( value = "Identifies the LRAId that the compensator joined", required = true )
+            @PathParam("LRAId") String lraId,
             @ApiParam( value = "An identifier that was returned by the coordinator when a compensator joined the LRA", required = true )
             @PathParam("RecCoordId") String rcvCoordId) throws NotFoundException {
 
@@ -77,25 +87,43 @@ public class RecoveryCoordinator {
     // Performing a PUT on the recovery URL will overwrite the old <compensor URL> with the new one supplied
     // and return the old url
     @PUT
-    @Path("{RecCoordId}")
+    @Path("{LRAId}/{RecCoordId}")
     @Produces(MediaType.APPLICATION_JSON)
-    @ApiOperation(value = "Lookup the compensator URL",
+    @ApiOperation(value = "Update the endpoint that a compensator is prepared to accept requests on.",
             notes = "Performing a PUT on the recovery URL will overwrite the old <compensor URL> with the new one supplied" +
-                    " and return the old url. The old value is returned",
+                    " and return the old url. The old value is returned." +
+                    "The full URL was returned when the compensator first joined the LRA.",
             response = String.class)
     @ApiResponses( {
             @ApiResponse( code = 404, message = "The coordinator has no knowledge of this compensator" ),
             @ApiResponse( code = 200, message = "The coordinator has replaced the old compensator with the new one " )
     } )
     public String replaceCompensator(
+            @ApiParam( value = "Identifies the LRAId that the compensator joined", required = true )
+            @PathParam("LRAId") String lraId,
             @ApiParam( value = "An identifier that was returned by the coordinator when a compensator joined the LRA", required = true )
-            @PathParam("RecCoordId")String rcvCoordId, String newCompensatorUrl) throws NotFoundException {
+            @PathParam("RecCoordId") String rcvCoordId,
+            String newCompensatorUrl) throws NotFoundException {
         String compensatorUrl = lraService.getParticipant(rcvCoordId);
 
         if (compensatorUrl != null) {
-            lraService.addCompensator(null, rcvCoordId, newCompensatorUrl);
+            URL lra = null;
 
-            return compensatorUrl;
+            try {
+                lra = new URL(lraId);
+            } catch (MalformedURLException e) {
+                throw new GenericLRAException(null,
+                        Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), e.getMessage(), e.getCause());
+            }
+
+            lraService.updateRecoveryURL(lra, newCompensatorUrl, rcvCoordId, true);
+
+            try {
+                return context.getRequestUri().toURL().toExternalForm();
+            } catch (MalformedURLException e) { // cannot happen
+                throw new GenericLRAException(lra,
+                        Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), e.getMessage(), e.getCause());
+            }
         }
 
         throw new NotFoundException(rcvCoordId);
