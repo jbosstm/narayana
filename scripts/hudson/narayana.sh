d616ad69ef95a13bf1f5c4e2772811bc220a9389
Ondra Chaloupka <ochaloup@redhat.com>
2018-02-07 08:27:57 +0100

    [TO DELETE] verbose scripts

--- scripts/hudson/narayana.sh
+++ scripts/hudson/narayana.sh
@@ -511,6 +511,8 @@ function xts_tests {
   grep async-registration "$CONF"
   sed -e 's#<[^<]*async-registration[^>]*>##g' $CONF > "$CONF.tmp" && mv "$CONF.tmp" "$CONF"
   sed -e 's#\(<subsystem.*xts.*\)#\1\n            <async-registration enabled="true"/>#' $CONF > "$CONF.tmp" && mv "$CONF.tmp" "$CONF"
+  echo "How is the xts configuration looks like?"
+  cat "$CONF"
 
   if [ $WSTX_MODULES ]; then
     [[ $WSTX_MODULES = *crash-recovery-tests* ]] || ran_crt=0
