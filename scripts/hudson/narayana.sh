f6047b16aac0c1b18403782cbcc3af2af9bbeb94
Tom Jenkinson <tom.jenkinson@redhat.com>
2018-07-25 12:13:41 +0100

    JBTM-3030 Moved Tomcat to https://github.com/web-servers/narayana-tomcat

--- scripts/hudson/narayana.sh
+++ scripts/hudson/narayana.sh
@@ -603,11 +603,13 @@ javax.sql.level = ALL
 org.apache.tomcat.tomcat-dbcp.level = ALL
 com.arjuna.level = ALL
 EOT
+    rm -rf narayana-tomcat
+    git clone https://github.com/jbosstm/narayana-tomcat.git
     echo "Executing Narayana Tomcat tests"
-    ./build.sh -f tomcat/tomcat-jta/pom.xml -B -P${ARQ_PROF}-tomcat ${CODE_COVERAGE_ARGS} -Dtest.db.type=h2 "$@" ${IPV6_OPTS} clean install "$@"
+    ./build.sh -f narayana-tomcat/pom.xml -B -P${ARQ_PROF}-tomcat ${CODE_COVERAGE_ARGS} -Dtest.db.type=h2 "$@" ${IPV6_OPTS} clean install "$@"
     RESULT=$?
     [ $RESULT = 0 ] || fatal "Narayana Tomcat tests failed H2"
-    ./build.sh -f tomcat/tomcat-jta/pom.xml -B -P${ARQ_PROF}-tomcat ${CODE_COVERAGE_ARGS} -Dtest.db.type=pgsql "$@" ${IPV6_OPTS} clean install "$@"
+    ./build.sh -f narayana-tomcat/pom.xml -B -P${ARQ_PROF}-tomcat ${CODE_COVERAGE_ARGS} -Dtest.db.type=pgsql "$@" ${IPV6_OPTS} clean install "$@"
     RESULT=$?
     [ $RESULT = 0 ] || fatal "Narayana Tomcat tests failed Postgres"
     rm -r ${CATALINA_HOME}
--- tomcat/README.md
+++
@@ -1,3 +0,0 @@
-# Tomcat Narayana testing
-
-The code has been moved to https://github.com/web-servers/narayana-tomcat
