5833d4170882a205b199924818950b162870df91
Michael Musgrove <mmusgrov@redhat.com>
2018-08-19 02:56:55 +0100

    JBTM-3049 arjPropertyManager should update all named config bean instances

--- ArjunaCore/arjuna/classes/com/arjuna/ats/arjuna/common/arjPropertyManager.java
+++ ArjunaCore/arjuna/classes/com/arjuna/ats/arjuna/common/arjPropertyManager.java
@@ -41,18 +41,40 @@ import com.arjuna.common.internal.util.propertyservice.BeanPopulator;
  */
 public class arjPropertyManager
 {
+    /*
+     * Note to implementers:
+     * if named instances of the CoreEnvironmentBean are created then
+     * you must apply the same technique as is used in the implementation
+     * of getObjectStoreEnvironmentBean method below
+     */
     public static CoreEnvironmentBean getCoreEnvironmentBean()
     {
         return BeanPopulator.getDefaultInstance(CoreEnvironmentBean.class);
     }
 
+    /*
+     * Note to implementers:
+     * if named instances of the CoordinatorEnvironmentBean are created then
+     * you must apply the same technique as is used in the implementation
+     * of getObjectStoreEnvironmentBean method below
+     */
     public static CoordinatorEnvironmentBean getCoordinatorEnvironmentBean()
     {
         return BeanPopulator.getDefaultInstance(CoordinatorEnvironmentBean.class);
     }
 
+    /**
+     * @return an ObjectStoreEnvironmentBean instance that will update all named
+     * instances of beans of type ObjectStoreEnvironmentBean as returned from calls
+     * to {@link BeanPopulator#getNamedInstance(Class, String)} and
+     * {@link BeanPopulator#getDefaultInstance(Class)}.
+     *
+     * The getters of the returned object will pass through to the getter of
+     * the default bean instance that is returned from a call to
+     * {@link BeanPopulator#getDefaultInstance(Class)}.
+     */
     public static ObjectStoreEnvironmentBean getObjectStoreEnvironmentBean()
     {
-        return BeanPopulator.getDefaultInstance(ObjectStoreEnvironmentBean.class);
+        return BeanPopulator.getDefaultInstance(MetaObjectStoreEnvironmentBean.class);
     }
 }
---
+++ ArjunaCore/arjuna/tests/classes/com/hp/mwtests/ts/arjuna/common/ArjPropertyManagerTest.java
@@ -0,0 +1,42 @@
+package com.hp.mwtests.ts.arjuna.common;
+
+import com.arjuna.ats.arjuna.common.ObjectStoreEnvironmentBean;
+import com.arjuna.ats.arjuna.common.arjPropertyManager;
+import com.arjuna.common.internal.util.propertyservice.BeanPopulator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ArjPropertyManagerTest {
+    @Test
+    public void test() {
+        BeanPopulator.getNamedInstance(ObjectStoreEnvironmentBean.class, "communicationStore").setObjectStoreSync(false);
+        assertFalse(BeanPopulator.getNamedInstance(ObjectStoreEnvironmentBean.class, "communicationStore").isObjectStoreSync());
+
+        BeanPopulator.getDefaultInstance(ObjectStoreEnvironmentBean.class).setObjectStoreSync(false);
+        assertFalse(arjPropertyManager.getObjectStoreEnvironmentBean().isObjectStoreSync());
+
+        BeanPopulator.getNamedInstance(ObjectStoreEnvironmentBean.class, "communicationStore").setObjectStoreSync(true);
+        assertTrue(BeanPopulator.getNamedInstance(ObjectStoreEnvironmentBean.class, "communicationStore").isObjectStoreSync());
+        assertFalse(BeanPopulator.getDefaultInstance(ObjectStoreEnvironmentBean.class).isObjectStoreSync());
+        assertFalse(arjPropertyManager.getObjectStoreEnvironmentBean().isObjectStoreSync());
+
+        BeanPopulator.getDefaultInstance(ObjectStoreEnvironmentBean.class).setObjectStoreSync(true);
+        assertTrue(arjPropertyManager.getObjectStoreEnvironmentBean().isObjectStoreSync());
+        assertTrue(BeanPopulator.getNamedInstance(ObjectStoreEnvironmentBean.class, "communicationStore").isObjectStoreSync());
+
+
+        arjPropertyManager.getObjectStoreEnvironmentBean().setObjectStoreSync(false);
+        assertFalse(BeanPopulator.getDefaultInstance(ObjectStoreEnvironmentBean.class).isObjectStoreSync());
+        assertFalse(BeanPopulator.getNamedInstance(ObjectStoreEnvironmentBean.class, "communicationStore").isObjectStoreSync());
+
+        arjPropertyManager.getObjectStoreEnvironmentBean().setObjectStoreSync(false);
+        assertFalse(BeanPopulator.getDefaultInstance(ObjectStoreEnvironmentBean.class).isObjectStoreSync());
+        assertFalse(BeanPopulator.getNamedInstance(ObjectStoreEnvironmentBean.class, "communicationStore").isObjectStoreSync());
+
+        arjPropertyManager.getObjectStoreEnvironmentBean().setObjectStoreSync(true);
+        assertTrue(BeanPopulator.getDefaultInstance(ObjectStoreEnvironmentBean.class).isObjectStoreSync());
+        assertTrue(BeanPopulator.getNamedInstance(ObjectStoreEnvironmentBean.class, "communicationStore").isObjectStoreSync());
+    }
+}
