97595cc80d7a2b481207377cd6b01534dedf1284
Ivan Straka <istraka@redhat.com>
2019-02-19 12:58:39 +0100

    postgres_driver ought to extend postgresql_driver

--- ArjunaCore/arjuna/classes/com/arjuna/ats/internal/arjuna/objectstore/jdbc/drivers/postgres_driver.java
+++ ArjunaCore/arjuna/classes/com/arjuna/ats/internal/arjuna/objectstore/jdbc/drivers/postgres_driver.java
@@ -20,33 +20,10 @@
  */
 package com.arjuna.ats.internal.arjuna.objectstore.jdbc.drivers;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
-import com.arjuna.ats.internal.arjuna.objectstore.jdbc.JDBCImple_driver;
 
 /**
  * JDBC store implementation driver-specific code. This version for Postgres
  * Plus JDBC Drivers.
  */
-public class postgres_driver extends JDBCImple_driver {
-
-	@Override
-	protected void checkCreateTableError(SQLException ex) throws SQLException {
-		if (!ex.getSQLState().equals("42P07")) {
-			throw ex;
-		}
-	}
-
-	@Override
-	protected void checkDropTableException(Connection connection,
-			SQLException ex) throws SQLException {
-		if (!ex.getSQLState().equals("42P01")) {
-			throw ex;
-		} else {
-			// For some reason PSQL leaves the transaction in a bad state on a
-			// failed drop
-			connection.commit();
-		}
-	}
+public class postgres_driver extends postgresql_driver {
 }
