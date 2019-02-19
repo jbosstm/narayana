6814dc498bac3c971a3154f1af815f15c562c2ef
Ivan Straka <istraka@redhat.com>
2019-02-19 14:28:21 +0100

    Updated javadoc for driver classes

--- ArjunaCore/arjuna/classes/com/arjuna/ats/internal/arjuna/objectstore/jdbc/drivers/edb_driver.java
+++ ArjunaCore/arjuna/classes/com/arjuna/ats/internal/arjuna/objectstore/jdbc/drivers/edb_driver.java
@@ -23,7 +23,7 @@ package com.arjuna.ats.internal.arjuna.objectstore.jdbc.drivers;
 
 /**
  * JDBC store implementation driver-specific code. This version for Postgres
- * Plus JDBC Drivers.
+ * Plus edb-jdbc18.jar JDBC Driver and later.
  */
 public class edb_driver extends postgres_driver {
 }
--- ArjunaCore/arjuna/classes/com/arjuna/ats/internal/arjuna/objectstore/jdbc/drivers/postgres_driver.java
+++ ArjunaCore/arjuna/classes/com/arjuna/ats/internal/arjuna/objectstore/jdbc/drivers/postgres_driver.java
@@ -23,7 +23,7 @@ package com.arjuna.ats.internal.arjuna.objectstore.jdbc.drivers;
 
 /**
  * JDBC store implementation driver-specific code. This version for Postgres
- * Plus JDBC Drivers.
+ * Plus edb-jdbc17.jar JDBC Driver and earlier.
  */
 public class postgres_driver extends postgresql_driver {
 }
