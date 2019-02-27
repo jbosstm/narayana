0f2c4f9fdd2e0f648222938002ae21047cefda43
Amos Feng <zfeng@redhat.com>
2019-02-27 15:07:36 +0800

    JBTM-3057 replace the 'tt' with the 'code' as it is not supported on HTML5

--- ArjunaJTA/jta/classes/com/arjuna/ats/jta/exceptions/RollbackException.java
+++ ArjunaJTA/jta/classes/com/arjuna/ats/jta/exceptions/RollbackException.java
@@ -81,7 +81,7 @@ public class RollbackException extends IllegalStateException
      *            {@link #getMessage()} method).
      * @param cause
      *            the cause (which is saved for later retrieval by the
-     *            {@link #getCause()} method). (A <tt>null</tt> value is
+     *            {@link #getCause()} method). (A <code>null</code> value is
      *            permitted, and indicates that the cause is nonexistent or
      *            unknown.)
      * @since 1.4
@@ -93,15 +93,15 @@ public class RollbackException extends IllegalStateException
 
     /**
      * Constructs a new exception with the specified cause and a detail message
-     * of <tt>(cause==null ? null : cause.toString())</tt> (which typically
-     * contains the class and detail message of <tt>cause</tt>). This
+     * of <code>(cause==null ? null : cause.toString())</code> (which typically
+     * contains the class and detail message of <code>cause</code>). This
      * constructor is useful for exceptions that are little more than wrappers
      * for other throwables (for example,
      * {@link java.security.PrivilegedActionException}).
      * 
      * @param cause
      *            the cause (which is saved for later retrieval by the
-     *            {@link #getCause()} method). (A <tt>null</tt> value is
+     *            {@link #getCause()} method). (A <code>null</code> value is
      *            permitted, and indicates that the cause is nonexistent or
      *            unknown.)
      * @since 1.4
