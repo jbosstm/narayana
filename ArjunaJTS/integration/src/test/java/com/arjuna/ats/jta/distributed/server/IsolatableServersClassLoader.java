6c9b1fe2b5ccd17208e17759abe2ac1d6a536a41
Ondra Chaloupka <ochaloup@redhat.com>
2019-06-05 12:51:26 +0200

    [JBTM-3153] race condition at SimpleIsolatedServers

--- ArjunaJTS/integration/src/test/java/com/arjuna/ats/jta/distributed/server/IsolatableServersClassLoader.java
+++ ArjunaJTS/integration/src/test/java/com/arjuna/ats/jta/distributed/server/IsolatableServersClassLoader.java
@@ -27,10 +27,8 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import sun.misc.Resource;
 import sun.misc.URLClassPath;
