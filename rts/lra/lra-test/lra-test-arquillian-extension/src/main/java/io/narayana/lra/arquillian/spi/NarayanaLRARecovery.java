bda9c9f7e9bc66873393cbb24b003fb41749a371
xstefank <xstefank122@gmail.com>
2019-10-01 21:35:41 +0200

    JBTM-3204 Upgrade LRA TCK recovery handling to accommodate specification changes

--- rts/lra/lra-test/lra-test-arquillian-extension/src/main/java/io/narayana/lra/arquillian/spi/NarayanaLRARecovery.java
+++ rts/lra/lra-test/lra-test-arquillian-extension/src/main/java/io/narayana/lra/arquillian/spi/NarayanaLRARecovery.java
@@ -1,29 +1,7 @@
-/*
- * JBoss, Home of Professional Open Source.
- * Copyright 2020, Red Hat, Inc., and individual contributors
- * as indicated by the @author tags. See the copyright.txt file in the
- * distribution for a full listing of individual contributors.
- *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- */
-
 package io.narayana.lra.arquillian.spi;
 
 import io.narayana.lra.LRAConstants;
-import io.narayana.lra.logging.LRALogger;
+import org.eclipse.microprofile.lra.tck.service.spi.LRARecoveryService;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -31,22 +9,14 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 import java.net.URI;
 
-public class NarayanaLRARecovery {
+public class NarayanaLRARecovery implements LRARecoveryService {
 
+    @Override
     public void waitForCallbacks(URI lraId) {
         // no action is needed, tck callbacks calls are sufficiently fast for TCK
     }
 
-    // TODO remove once this class implements LRARecoveryService
-    public void waitForRecovery(URI lraId) {
-        int counter = 0;
-
-        do {
-            LRALogger.logger.info("Recovery attempt #" + ++counter);
-        } while (!waitForEndPhaseReplay(lraId));
-        LRALogger.logger.info("LRA " + lraId + "has finished the recovery " + counter);
-    }
-
+    @Override
     public boolean waitForEndPhaseReplay(URI lraId) {
         String host = lraId.getHost();
         int port = lraId.getPort();
@@ -80,6 +50,7 @@ public class NarayanaLRARecovery {
             Response response = recoveryTarget.request().get();
             String json = response.readEntity(String.class);
             response.close();
+
 
             if (json.contains(lraId.toASCIIString())) {
                 // intended LRA didn't recover
