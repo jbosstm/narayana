bd0d7706604e4a264c28ac135331e0bb86cc2422
Michael Musgrove <mmusgrov@redhat.com>
2019-10-28 11:26:25 +0000

    Updated to 5.10.1.Final-SNAPSHOT

--- blacktie/btadmin/src/main/java/org/jboss/narayana/blacktie/btadmin/commands/Version.java
+++ blacktie/btadmin/src/main/java/org/jboss/narayana/blacktie/btadmin/commands/Version.java
@@ -43,6 +43,6 @@ public class Version implements Command {
     }
 
     public void invoke(BlacktieAdministration connection, Properties configuration) {
-        log.info("JBoss BlackTie 5.10.0.Final");
+        log.info("JBoss BlackTie 5.10.1.Final-SNAPSHOT");
     }
 }
