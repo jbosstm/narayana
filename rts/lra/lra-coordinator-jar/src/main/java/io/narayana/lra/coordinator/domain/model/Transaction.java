448350f68f9bc746df905bbe4ec1d3e2ff770520
xstefank <xstefank122@gmail.com>
2019-12-06 13:02:52 +0100

    JBTM-3229 Allow to enlist AfterLRA listeners after the LRA enters the ending phase

--- rts/lra/lra-coordinator-jar/src/main/java/io/narayana/lra/coordinator/domain/model/Transaction.java
+++ rts/lra/lra-coordinator-jar/src/main/java/io/narayana/lra/coordinator/domain/model/Transaction.java
@@ -568,6 +568,12 @@ public class Transaction extends AtomicAction {
             }
 
             return p;
+        } else if (isRecovering() && p.getCompensator() == null && p.getEndNotificationUri() != null) {
+            // the participant is an AfterLRA listener
+            afterLRAListeners.add(new AfterLRAListener(p.getEndNotificationUri(), p.getRecoveryCoordinatorURI()));
+            updateState();
+
+            return p;
         }
 
         return null;
