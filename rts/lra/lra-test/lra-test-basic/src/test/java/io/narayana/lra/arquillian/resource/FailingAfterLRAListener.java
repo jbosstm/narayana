6a3feaa36697049764828a74c794fed7dcb70ccd
Michael Musgrove <mmusgrov@redhat.com>
2020-01-25 12:53:04 +0000

    JBTM-3239 Test cherry-picked from xstefank's repo

---
+++ rts/lra/lra-test/lra-test-basic/src/test/java/io/narayana/lra/arquillian/resource/FailingAfterLRAListener.java
@@ -0,0 +1,67 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2019, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package io.narayana.lra.arquillian.resource;
+
+import org.eclipse.microprofile.lra.annotation.AfterLRA;
+import org.eclipse.microprofile.lra.annotation.ws.rs.LRA;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Path(FailingAfterLRAListener.ROOT_PATH)
+public class FailingAfterLRAListener {
+
+    public static final String ROOT_PATH = "failing-after-lra-listener";
+    public static final String ACTION_PATH = "action";
+
+    private static final AtomicInteger afterLRACounter = new AtomicInteger(0);
+
+    @GET
+    @Path(ACTION_PATH)
+    @LRA(LRA.Type.REQUIRED)
+    public String doWorkInLRA(@HeaderParam(LRA.LRA_HTTP_CONTEXT_HEADER) URI lraId) {
+        return lraId.toASCIIString();
+    }
+
+    @PUT
+    @Path("after-lra")
+    @AfterLRA
+    public Response afterLRA(@HeaderParam(LRA.LRA_HTTP_ENDED_CONTEXT_HEADER) URI endedLRAId) {
+        if (afterLRACounter.getAndIncrement() < 2) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+
+        return Response.ok().build();
+    }
+
+    @GET
+    @Path("counter")
+    public Response getCounterValue() {
+        return Response.ok(afterLRACounter.get()).build();
+    }
+}
