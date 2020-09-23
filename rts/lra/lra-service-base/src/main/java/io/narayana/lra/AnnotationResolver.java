c46bcc0a9d8f09c7f81bd391f1d2c518c46a910d
xstefank <xstefank122@gmail.com>
2020-09-23 14:39:07 +0200

    JBTM-3360 ServerLRAFilter is relying on the wrong RESTEasy behavior

--- rts/lra/lra-service-base/src/main/java/io/narayana/lra/AnnotationResolver.java
+++ rts/lra/lra-service-base/src/main/java/io/narayana/lra/AnnotationResolver.java
@@ -19,7 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package io.narayana.lra.client.internal.proxy.nonjaxrs;
+package io.narayana.lra;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
