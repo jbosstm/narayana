04ddccd1022e45b81d208df2b2190017cf442079
Ondra Chaloupka <ochaloup@redhat.com>
2020-11-30 09:55:21 +0100

    [JBTM-3400] processing WFLY modules classpath with LRA Jandex indexer

--- rts/lra/proxy/api/src/main/java/io/narayana/lra/client/internal/proxy/nonjaxrs/ClassPathIndexer.java
+++ rts/lra/proxy/api/src/main/java/io/narayana/lra/client/internal/proxy/nonjaxrs/ClassPathIndexer.java
@@ -3,6 +3,9 @@ package io.narayana.lra.client.internal.proxy.nonjaxrs;
 import io.narayana.lra.logging.LRALogger;
 import org.jboss.jandex.Index;
 import org.jboss.jandex.Indexer;
+import org.jboss.logging.Logger;
+import org.jboss.modules.ModuleClassLoader;
+import org.jboss.modules.Resource;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,11 +16,13 @@ import java.net.URLClassLoader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 class ClassPathIndexer {
+    private static final Logger log = Logger.getLogger(ClassPathIndexer.class);
 
     /**
      * Creates Jandex index from the application classpath
@@ -26,9 +31,11 @@ class ClassPathIndexer {
         Indexer indexer = new Indexer();
         List<URL> urls;
 
+        // System Class Loader based on URL class path
         ClassLoader cl = ClassLoader.getSystemClassLoader();
+
         if (cl instanceof URLClassLoader) {
-            urls = Arrays.asList(((URLClassLoader) cl).getURLs());
+            urls = new ArrayList<>(Arrays.asList(((URLClassLoader) cl).getURLs()));
         } else {
             urls = collectURLsFromClassPath();
         }
@@ -36,6 +43,8 @@ class ClassPathIndexer {
         for (URL url : urls) {
             processFile(url.openStream(), indexer);
         }
+
+        tryToIndexContextModuleClassLoader(indexer);
 
         return indexer.complete();
     }
@@ -67,6 +76,29 @@ class ClassPathIndexer {
             } else if (entryName.endsWith(".war")) {
                 // necessary because of the thorntail arquillian adapter
                 processFile(zis, indexer);
+            }
+        }
+    }
+
+    private void tryToIndexContextModuleClassLoader(Indexer indexer) throws IOException {
+        try {
+            this.getClass().forName("org.jboss.modules.ModuleClassLoader");
+        } catch (ClassNotFoundException cnfe) {
+            log.debug("Cannot load class 'org.jboss.modules.ModuleClassLoader' to index resources", cnfe);
+            return;
+        }
+
+        // ModuleClassLoader is available at the classpath and we can try to use it to list loaded resources
+        ClassLoader currentThreadClassLoader = Thread.currentThread().getContextClassLoader();
+        if(currentThreadClassLoader instanceof ModuleClassLoader) {
+            Iterator<Resource> resources = ((ModuleClassLoader) currentThreadClassLoader).iterateResources("", true);
+            while (resources.hasNext()) {
+                Resource resource = resources.next();
+                if (resource.getURL().getFile().endsWith("class")) {
+                    try (InputStream is = resource.openStream()) {
+                        indexer.index(resource.openStream());
+                    }
+                }
             }
         }
     }
