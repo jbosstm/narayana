cceeba1ec70b74ddaa50a3861acaee2cba723abd
Ondra Chaloupka <ochaloup@redhat.com>
2021-03-19 13:45:06 +0100

    Updated to 5.11.1.Final-SNAPSHOT

--- scripts/hudson/narayana.bat
+++ scripts/hudson/narayana.bat
@@ -43,7 +43,7 @@ rem REPLACE THE OPENJDK-ORB WITH THE 8.0.8.Final
 call wget --no-check-certificate https://repository.jboss.org/nexus/content/repositories/releases/org/jboss/openjdk-orb/openjdk-orb/8.0.8.Final/openjdk-orb-8.0.8.Final.jar -O %JBOSS_HOME%\modules\system\layers\base\javax\orb\api\main\openjdk-orb-8.0.8.Final.jar || (call:fail_build & exit -1)
 set OPENJDK_ORB_MODULE_XML=%JBOSS_HOME%\modules\system\layers\base\javax\orb\api\main\module.xml
 powershell -Command "((Get-Content ($env:OPENJDK_ORB_MODULE_XML)).replace('8.0.6.Final', '8.0.8.Final') | Set-Content ($env:OPENJDK_ORB_MODULE_XML))"
-unzip wildfly-blacktie\build\target\wildfly-blacktie-build-5.11.0.Final-bin.zip -d %JBOSS_HOME%
+unzip wildfly-blacktie\build\target\wildfly-blacktie-build-5.11.1.Final-SNAPSHOT-bin.zip -d %JBOSS_HOME%
 cd ..\
 
 rem INITIALIZE JBOSS
