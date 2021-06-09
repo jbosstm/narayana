0e75da61eb3feb5108e590935c3b97a6d05b1d47
Ondra Chaloupka <ochaloup@redhat.com>
2021-06-09 08:39:28 +0200

    Updated to 5.12.0.Final

--- scripts/hudson/narayana.sh
+++ scripts/hudson/narayana.sh
@@ -52,7 +52,7 @@ function get_pull_xargs {
 function init_test_options {
     is_ibm
     ISIBM=$?
-    [ $NARAYANA_CURRENT_VERSION ] || NARAYANA_CURRENT_VERSION="5.11.3.Final-SNAPSHOT"
+    [ $NARAYANA_CURRENT_VERSION ] || NARAYANA_CURRENT_VERSION="5.12.0.Final"
     [ $CODE_COVERAGE ] || CODE_COVERAGE=0
     [ x"$CODE_COVERAGE_ARGS" != "x" ] || CODE_COVERAGE_ARGS=""
     [ $ARQ_PROF ] || ARQ_PROF=arq	# IPv4 arquillian profile
