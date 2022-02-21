facc4b81e2225978b90f792298bb008750673599
Mayank Kunwar <msingh722@gmail.com>
2022-02-22 02:25:48 +0530

    updated byteman and replaced maven-injection-plugin by maven-resources-plugin

---
+++ common/tests/com/arjuna/common/tests/ConfigurationInfoTest.java
@@ -0,0 +1,31 @@
+package com.arjuna.common.tests;
+
+import com.arjuna.common.util.ConfigurationInfo;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ConfigurationInfoTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testBuildId() {
+        String buildId = System.getProperty("test_build_id");
+        assertEquals(buildId, ConfigurationInfo.getBuildId());
+    }
+
+    @Test
+    public void testPropertyFileName() {
+        String propFileName = System.getProperty("test_build_prop_file");
+        assertEquals(propFileName, ConfigurationInfo.getPropertiesFile());
+    }
+
+    @Test
+    public void testVersion() {
+        String version = System.getProperty("test_build_version");
+        assertEquals(version, ConfigurationInfo.getVersion());
+    }
+}
