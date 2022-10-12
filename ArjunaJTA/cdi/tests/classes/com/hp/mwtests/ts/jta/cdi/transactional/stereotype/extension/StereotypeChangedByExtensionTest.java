fe4b0d2afaf26c9199f6f22304783c14534cbbfe
Marco Sappé Griot <msappegr@redhat.com>
2022-10-12 13:15:13 +0200

    [JBTM-3588] jakarta migration, bean auto-discovery set to all
    see https://jakarta.ee/specifications/cdi/4.0/jakarta-cdi-spec-4.0.html#_jakarta_contexts_and_dependency_injection_4_0

--- ArjunaJTA/cdi/tests/classes/com/hp/mwtests/ts/jta/cdi/transactional/stereotype/extension/StereotypeChangedByExtensionTest.java
+++ ArjunaJTA/cdi/tests/classes/com/hp/mwtests/ts/jta/cdi/transactional/stereotype/extension/StereotypeChangedByExtensionTest.java
@@ -27,7 +27,7 @@ import jakarta.inject.Inject;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -44,7 +44,7 @@ public class StereotypeChangedByExtensionTest {
         return ShrinkWrap.create(WebArchive.class, "stereotype-test.war")
             .addPackage(StereotypeChangedByExtensionTest.class.getPackage())
             .addClasses(TransactionalRequiredStereotype.class)
-            .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+            .addAsWebInfResource(new StringAsset("<beans bean-discovery-mode=\"all\"></beans>"), "beans.xml")
             .addAsServiceProvider(jakarta.enterprise.inject.spi.Extension.class, AddStereotypeAnnotationExtension.class);
     }
 
