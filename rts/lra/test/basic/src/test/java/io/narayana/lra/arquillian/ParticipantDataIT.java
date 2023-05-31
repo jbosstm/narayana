eb37cf829dc9347b2e63f1ff9092fe39db692bf7
Marco Sappé Griot <msappegr@redhat.com>
2023-05-31 12:38:26 +0200

    JBTM-3755 converting StringBuilder into String
    wip

--- rts/lra/test/basic/src/test/java/io/narayana/lra/arquillian/ParticipantDataIT.java
+++ rts/lra/test/basic/src/test/java/io/narayana/lra/arquillian/ParticipantDataIT.java
@@ -11,6 +11,8 @@ import io.narayana.lra.arquillian.resource.LRAParticipantWithStatusURI;
 import io.narayana.lra.arquillian.resource.LRAParticipantWithoutStatusURI;
 import io.narayana.lra.arquillian.resource.ParticipantDataResource2;
 import io.narayana.lra.arquillian.spi.NarayanaLRARecovery;
+import jakarta.ws.rs.core.MultivaluedHashMap;
+import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.lra.tck.service.spi.LRACallbackException;
 import org.jboss.arquillian.container.test.api.Deployment;
@@ -27,6 +29,7 @@ import java.net.URISyntaxException;
 import java.net.URL;
 
 import static org.eclipse.microprofile.lra.annotation.ws.rs.LRA.LRA_HTTP_CONTEXT_HEADER;
+import static io.narayana.lra.LRAConstants.NARAYANA_LRA_PARTICIPANT_DATA_HEADER_NAME;
 
 /**
  * There is a spec requirement to report failed LRAs but the spec only requires that a failure message is reported
@@ -62,7 +65,7 @@ public class ParticipantDataIT extends TestBase {
     @Test
     public void testSimple() throws LRACallbackException, URISyntaxException {
         URI lraId = new URI(invoke(null, ParticipantDataResource.SIMPLE_PARTICIPANT_RESOURCE_PATH,
-                ParticipantDataResource.START_LRA_PATH, Response.Status.OK.getStatusCode()));
+                ParticipantDataResource.START_LRA_PATH, Response.Status.OK.getStatusCode(), null));
 
         lrasToAfterFinish.add(lraId);
 
@@ -70,7 +73,7 @@ public class ParticipantDataIT extends TestBase {
         new NarayanaLRARecovery().waitForRecovery(lraId);
 
         String calls = invoke(null, ParticipantDataResource.SIMPLE_PARTICIPANT_RESOURCE_PATH,
-                ParticipantDataResource.CALLS_PATH, Response.Status.OK.getStatusCode());
+                ParticipantDataResource.CALLS_PATH, Response.Status.OK.getStatusCode(), null);
 
         Assert.assertTrue("missing Complete callback: " + calls, calls.contains("@Complete"));
         Assert.assertTrue("missing Status callback: " + calls, calls.contains("@Status"));
@@ -82,22 +85,30 @@ public class ParticipantDataIT extends TestBase {
     public void testPropagateLRAData() throws URISyntaxException {
         // invoke a resource that starts an LRA
         URI lraId = new URI(invoke(null, ParticipantDataResource2.DATA_PARTICIPANT_RESOURCE_PATH,
-                ParticipantDataResource2.START_LRA_PATH, Response.Status.OK.getStatusCode()));
+                ParticipantDataResource2.START_LRA_PATH, Response.Status.OK.getStatusCode(), ParticipantDataResource2.START_DATA));
         // invoke a second resource in the same context
         String data = invoke(lraId, ParticipantDataResource2.DATA_PARTICIPANT_RESOURCE_PATH,
-                ParticipantDataResource2.END_LRA_PATH, Response.Status.OK.getStatusCode());
+                ParticipantDataResource2.END_LRA_PATH, Response.Status.OK.getStatusCode(), ParticipantDataResource2.END_DATA);
 
         lrasToAfterFinish.add(lraId);
 
         Assert.assertEquals("wrong data in bean on second call", ParticipantDataResource2.START_DATA, data);
     }
 
-    private String invoke(URI lraId, String resourcePrefix, String resourcePath, int expectedStatus) {
+    private String invoke(URI lraId, String resourcePrefix, String resourcePath, int expectedStatus, String data) {
+
+        MultivaluedMap<String, Object> headers = new MultivaluedHashMap<>();
+        headers.add(LRA_HTTP_CONTEXT_HEADER, lraId);
+
+        if(data!= null) {
+            headers.add(NARAYANA_LRA_PARTICIPANT_DATA_HEADER_NAME, data);
+        }
+
         try (Response response = client.target(baseURL.toURI())
                 .path(resourcePrefix)
                 .path(resourcePath)
                 .request()
-                .header(LRA_HTTP_CONTEXT_HEADER, lraId)
+                .headers(headers)
                 .get()) {
 
             Assert.assertEquals(
