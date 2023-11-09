2baf441ed539c6f1f9df711e5ae9af7b138e99fc
Marco Sappé Griot <msappegr@redhat.com>
2023-11-09 19:19:56 +0100

    [RELEASE] change the scripts to follow the 6.0 branch release processing
    note: followed the previous commit https://github.com/jbosstm/narayana/commit/4568dd6ceb80ef73a40857104a236695f2e574c0

--- scripts/hudson/narayana-rebase.bat
+++ scripts/hudson/narayana-rebase.bat
@@ -1,7 +1,7 @@
 git rebase --abort
 git clean -f -d -x
-git branch -D 4.17
-git branch 4.17 origin/4.17
+git branch -D 6.0
+git branch 6.0 origin/6.0
 git branch -D main
 git branch main origin/main
 git pull --rebase --ff-only origin main
