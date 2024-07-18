2681694d066b082f31180dfdaae65edd09602417
Manuel Finelli <jfinelli@redhat.com>
2024-07-18 17:13:42 +0100

    [JBTM-3899] Fixed getNumberOfInflightTransactions

--- ArjunaCore/arjuna/classes/com/arjuna/ats/arjuna/coordinator/ActionManager.java
+++ ArjunaCore/arjuna/classes/com/arjuna/ats/arjuna/coordinator/ActionManager.java
@@ -39,9 +39,29 @@ public class ActionManager
 		_allActions.remove(id);
 	}
 
-    public int getNumberOfInflightTransactions()
+	/**
+	 * <p>
+	 *     This method works out the number of in-flight transactions currently in
+	 *     the system.
+	 * <p>
+	 *     Note: The definition of in-flight (a.k.a. active) transactions can be
+	 *     found in the OMG OTS specification: "[Active is defined as] the state
+	 *     of a transaction when processing is in progress and completion of the
+	 *     transaction has not yet commenced." In addition to the OMG OTS
+	 *     definition, transactions that are marked as abort-only are not
+	 *     considered active.
+	 *     In Arjuna, these considerations are distilled into the following:
+	 *     in-flight transactions are those BasicActions with status() equal to
+	 *     ActionStatus.RUNNING.
+	 *
+	 * @return the number of in-flight transactions currently in the system
+	 */
+	  public int getNumberOfInflightTransactions()
     {
-        return _allActions.size();
+        return (int) _allActions.entrySet()
+          .parallelStream()
+          .filter(entry -> entry.getValue().status() == ActionStatus.RUNNING)
+          .count();
     }
 	
 	private ActionManager()
--- ArjunaCore/arjuna/tests/classes/com/hp/mwtests/ts/arjuna/atomicaction/TxStatsUnitTest.java
+++ ArjunaCore/arjuna/tests/classes/com/hp/mwtests/ts/arjuna/atomicaction/TxStatsUnitTest.java
@@ -5,20 +5,25 @@
 
 package com.hp.mwtests.ts.arjuna.atomicaction;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
-import org.junit.Test;
-
 import com.arjuna.ats.arjuna.AtomicAction;
 import com.arjuna.ats.arjuna.common.arjPropertyManager;
+import com.arjuna.ats.arjuna.coordinator.TwoPhaseOutcome;
 import com.arjuna.ats.arjuna.coordinator.TxStats;
+import com.hp.mwtests.ts.arjuna.resources.BasicRecord;
+import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class TxStatsUnitTest
 {
+    @Before
+    public void before() {
+        Assertions.assertTrue(TxStats.enabled());
+    }
+
     @Test
     public void test() throws Exception
     {
@@ -31,9 +36,13 @@ public class TxStatsUnitTest
             
             A.begin();
             B.begin();
+
+            Assertions.assertEquals(2, TxStats.getInstance().getNumberOfInflightTransactions());
             
             B.commit();
             A.commit();
+
+            Assertions.assertEquals(0, TxStats.getInstance().getNumberOfInflightTransactions());
         }
         
         for (int i = 0; i < 100; i++)
@@ -41,33 +50,36 @@ public class TxStatsUnitTest
             AtomicAction A = new AtomicAction();
             
             A.begin();
+
+            Assertions.assertEquals(1, TxStats.getInstance().getNumberOfInflightTransactions());
             
             A.abort();
+
+            Assertions.assertEquals(0, TxStats.getInstance().getNumberOfInflightTransactions());
         }
-        
-        AtomicAction B = new AtomicAction();
-        
-        B.begin();
-        
-        assertTrue(TxStats.enabled());
-        assertEquals(100, TxStats.getInstance().getNumberOfAbortedTransactions());
-        assertEquals(100, TxStats.getInstance().getNumberOfApplicationRollbacks());
-        assertEquals(200, TxStats.getInstance().getNumberOfCommittedTransactions());
-        assertEquals(0, TxStats.getInstance().getNumberOfHeuristics());
-        assertEquals(1, TxStats.getInstance().getNumberOfInflightTransactions());
-        assertEquals(100, TxStats.getInstance().getNumberOfNestedTransactions());
-        assertEquals(0, TxStats.getInstance().getNumberOfResourceRollbacks());
-        assertEquals(0, TxStats.getInstance().getNumberOfTimedOutTransactions());
-        assertEquals(301, TxStats.getInstance().getNumberOfTransactions());
+
+        Assertions.assertEquals(100, TxStats.getInstance().getNumberOfAbortedTransactions());
+        Assertions.assertEquals(100, TxStats.getInstance().getNumberOfApplicationRollbacks());
+        Assertions.assertEquals(200, TxStats.getInstance().getNumberOfCommittedTransactions());
+        Assertions.assertEquals(0, TxStats.getInstance().getNumberOfHeuristics());
+        Assertions.assertEquals(100, TxStats.getInstance().getNumberOfNestedTransactions());
+        Assertions.assertEquals(0, TxStats.getInstance().getNumberOfResourceRollbacks());
+        Assertions.assertEquals(0, TxStats.getInstance().getNumberOfTimedOutTransactions());
+        Assertions.assertEquals(300, TxStats.getInstance().getNumberOfTransactions());
         
         PrintWriter pw = new PrintWriter(new StringWriter());
         
         TxStats.getInstance().printStatus(pw);
 
-        // https://issues.jboss.org/browse/JBTM-2643
         for (int i = 0; i < 100; i++) {
             final AtomicAction D = new AtomicAction();
             D.begin();
+
+            D.add(new StatsResource());
+            D.add(new StatsResource());
+
+            Assertions.assertEquals(1, TxStats.getInstance().getNumberOfInflightTransactions());
+
             Thread t1 = new Thread() {
                 @Override
                 public void run() {
@@ -84,10 +96,80 @@ public class TxStatsUnitTest
             t2.start();
             t1.join();
             t2.join();
+            D.removeThread();
         }
 
-        assertEquals(200, TxStats.getInstance().getNumberOfAbortedTransactions());
-        assertEquals(200, TxStats.getInstance().getNumberOfApplicationRollbacks());
-        assertEquals(401, TxStats.getInstance().getNumberOfTransactions());
+        Assertions.assertEquals(200, TxStats.getInstance().getNumberOfAbortedTransactions());
+        Assertions.assertEquals(200, TxStats.getInstance().getNumberOfApplicationRollbacks());
+        Assertions.assertEquals(400, TxStats.getInstance().getNumberOfTransactions());
+
+        for (int i = 0; i < 100; i++)
+        {
+            AtomicAction A = new AtomicAction();
+
+            A.begin();
+
+            // Let's add some resources
+            A.add(new StatsResource());
+            A.add(new StatsResource());
+
+            Assertions.assertEquals(1, TxStats.getInstance().getNumberOfInflightTransactions());
+
+            A.abort();
+
+            Assertions.assertEquals(0, TxStats.getInstance().getNumberOfInflightTransactions());
+        }
+
+        Assertions.assertEquals(300, TxStats.getInstance().getNumberOfAbortedTransactions());
+        Assertions.assertEquals(300, TxStats.getInstance().getNumberOfApplicationRollbacks());
+        Assertions.assertEquals(500, TxStats.getInstance().getNumberOfTransactions());
+
+        for (int i = 0; i < 100; i++)
+        {
+            AtomicAction A = new AtomicAction();
+
+            A.begin();
+
+            A.add(new StatsResource());
+            A.add(new StatsResource());
+
+            Assertions.assertEquals(1, TxStats.getInstance().getNumberOfInflightTransactions());
+
+            A.preventCommit();
+            A.commit();
+
+            Assertions.assertEquals(0, TxStats.getInstance().getNumberOfInflightTransactions());
+        }
+
+        Assertions.assertEquals(400, TxStats.getInstance().getNumberOfAbortedTransactions());
+        Assertions.assertEquals(300, TxStats.getInstance().getNumberOfApplicationRollbacks());
+        Assertions.assertEquals(600, TxStats.getInstance().getNumberOfTransactions());
+    }
+
+    @Test
+    public void testRollbackOnlyStat() {
+        AtomicAction A = new AtomicAction();
+        A.begin();
+        Assertions.assertEquals(1, TxStats.getInstance().getNumberOfInflightTransactions());
+        A.preventCommit();
+        Assertions.assertEquals(0, TxStats.getInstance().getNumberOfInflightTransactions());
+        A.commit();
+    }
+
+    private static class StatsResource extends BasicRecord {
+        
+        @Override
+        public int topLevelPrepare() {
+            // TxStats.getNumberOfInflightTransactions() shouldn't count pending transactions 
+            Assertions.assertEquals(0, TxStats.getInstance().getNumberOfInflightTransactions());
+            return TwoPhaseOutcome.PREPARE_OK;
+        }
+
+        @Override
+        public int topLevelAbort() {
+            // TxStats.getNumberOfInflightTransactions() shouldn't count aborting transactions 
+            Assertions.assertEquals(0, TxStats.getInstance().getNumberOfInflightTransactions());
+            return TwoPhaseOutcome.FINISH_OK;
+        }
     }
 }
