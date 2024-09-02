5475884ea3e762e413e6294adc159c775ea1ce6e
Manuel Finelli <jfinelli@redhat.com>
2024-09-02 19:55:46 +0100

    completed failure_recovery

---
+++ src/main/asciidoc/product/failure_recovery/extras/XAResourceRecoveryHelper.java
@@ -0,0 +1,6 @@
+public interface com.arjuna.ats.jta.recovery.
+
+XAResourceRecoveryHelper {
+    public boolean initialise (String p) throws Exception;
+    public XAResource[] getXAResources () throws Exception;
+}
