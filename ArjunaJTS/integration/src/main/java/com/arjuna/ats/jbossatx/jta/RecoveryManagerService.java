2f0821ef2151c19bc745c65af805b3679f12d9e5
Manuel Finelli <jfinelli@redhat.com>
2025-04-29 10:04:15 +0100

    [STYLE] RecoveryManagerService reformatted

--- ArjunaJTS/integration/src/main/java/com/arjuna/ats/jbossatx/jta/RecoveryManagerService.java
+++ ArjunaJTS/integration/src/main/java/com/arjuna/ats/jbossatx/jta/RecoveryManagerService.java
@@ -24,12 +24,10 @@ import com.arjuna.common.util.ConfigurationInfo;
  * @author Jonathan Halliday (jonathan.halliday@redhat.com)
  * @version $Id$
  */
-public class RecoveryManagerService implements XAResourceRecoveryRegistry
-{
+public class RecoveryManagerService implements XAResourceRecoveryRegistry {
     private RecoveryManager _recoveryManager;
 
-    public void create()
-    {
+    public void create() {
         String tag = ConfigurationInfo.getSourceId();
 
         jbossatxLogger.i18NLogger.info_jta_RecoveryManagerService_create(tag);
@@ -39,96 +37,89 @@ public class RecoveryManagerService implements XAResourceRecoveryRegistry
         _recoveryManager = RecoveryManager.manager();
     }
 
-    public void destroy()
-    {
+    public void destroy() {
     }
 
-    public void start()
-    {
+    public void start() {
         jbossatxLogger.i18NLogger.info_jta_RecoveryManagerService_start();
 
         _recoveryManager.initialize();
-        _recoveryManager.startRecoveryManagerThread() ;
+        _recoveryManager.startRecoveryManagerThread();
     }
 
-    public void stop() throws Exception
-    {
+    public void stop() throws Exception {
         jbossatxLogger.i18NLogger.info_jta_RecoveryManagerService_stop();
 
         _recoveryManager.terminate();
     }
 
-    public void suspend()
-    {
+    public void suspend() {
         jbossatxLogger.i18NLogger.info_jta_RecoveryManagerService_suspend();
         _recoveryManager.suspend(false);
     }
 
-    public void resume()
-    {
+    public void resume() {
         jbossatxLogger.i18NLogger.info_jta_RecoveryManagerService_resume();
         _recoveryManager.resume();
     }
 
     //////////////////////////////
 
-    public void addXAResourceRecovery(XAResourceRecovery xaResourceRecovery)
-    {
-        if(_recoveryManager == null) {
+    public void addXAResourceRecovery(XAResourceRecovery xaResourceRecovery) {
+        if (_recoveryManager == null) {
             throw new IllegalStateException(jbossatxLogger.i18NLogger.get_jta_RecoveryManagerService_norecoverysystem());
         }
 
         XARecoveryModule xaRecoveryModule = null;
-        for(RecoveryModule recoveryModule : ((Vector<RecoveryModule>)_recoveryManager.getModules())) {
-            if(recoveryModule instanceof XARecoveryModule) {
-                xaRecoveryModule = (XARecoveryModule)recoveryModule;
+        for (RecoveryModule recoveryModule : ((Vector<RecoveryModule>) _recoveryManager.getModules())) {
+            if (recoveryModule instanceof XARecoveryModule) {
+                xaRecoveryModule = (XARecoveryModule) recoveryModule;
                 break;
             }
         }
 
-        if(xaRecoveryModule == null) {
+        if (xaRecoveryModule == null) {
             throw new IllegalStateException(jbossatxLogger.i18NLogger.get_jta_RecoveryManagerService_norecoverymodule());
         }
 
         xaRecoveryModule.addXAResourceRecoveryHelper(new XAResourceRecoveryHelperWrapper(xaResourceRecovery));
     }
 
-    public void removeXAResourceRecovery(XAResourceRecovery xaResourceRecovery)
-    {
-        if(_recoveryManager == null) {
+    public void removeXAResourceRecovery(XAResourceRecovery xaResourceRecovery) {
+        if (_recoveryManager == null) {
             throw new IllegalStateException(jbossatxLogger.i18NLogger.get_jta_RecoveryManagerService_norecoverysystem());
         }
 
         XARecoveryModule xaRecoveryModule = null;
-        for(RecoveryModule recoveryModule : ((Vector <RecoveryModule>)_recoveryManager.getModules())) {
-            if(recoveryModule instanceof XARecoveryModule) {
-                xaRecoveryModule = (XARecoveryModule)recoveryModule;
+        for (RecoveryModule recoveryModule : ((Vector<RecoveryModule>) _recoveryManager.getModules())) {
+            if (recoveryModule instanceof XARecoveryModule) {
+                xaRecoveryModule = (XARecoveryModule) recoveryModule;
                 break;
             }
         }
 
-        if(xaRecoveryModule == null) {
+        if (xaRecoveryModule == null) {
             throw new IllegalStateException(jbossatxLogger.i18NLogger.get_jta_RecoveryManagerService_norecoverymodule());
         }
 
         xaRecoveryModule.removeXAResourceRecoveryHelper(new XAResourceRecoveryHelperWrapper(xaResourceRecovery));
     }
 
-	public void addSerializableXAResourceDeserializer(SerializableXAResourceDeserializer serializableXAResourceDeserializer) {
+    public void addSerializableXAResourceDeserializer(SerializableXAResourceDeserializer serializableXAResourceDeserializer) {
 
         XARecoveryModule xaRecoveryModule = null;
-        for(RecoveryModule recoveryModule : ((Vector<RecoveryModule>)_recoveryManager.getModules())) {
-            if(recoveryModule instanceof XARecoveryModule) {
-                xaRecoveryModule = (XARecoveryModule)recoveryModule;
+        for (RecoveryModule recoveryModule : ((Vector<RecoveryModule>) _recoveryManager.getModules())) {
+            if (recoveryModule instanceof XARecoveryModule) {
+                xaRecoveryModule = (XARecoveryModule) recoveryModule;
                 break;
             }
         }
 
-        if(xaRecoveryModule == null) {
+        if (xaRecoveryModule == null) {
             throw new IllegalStateException(jbossatxLogger.i18NLogger.get_jta_RecoveryManagerService_norecoverymodule());
         }
 
         xaRecoveryModule.addSerializableXAResourceDeserializer(serializableXAResourceDeserializer);
-		
-	}
+
+    }
 }
