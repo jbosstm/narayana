5460edc23f5b2877edaa1949e969ad17474e2238
Manuel Finelli <jfinelli@redhat.com>
2025-08-28 11:46:46 +0100

    [JBTM-3997 AND JBTM-3998] Narayana release process updated

---
+++ scripts/post-release.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+. pre-release-vars.sh
+
+function fatal {
+  echo "Failed: $1"
+  exit 1
+}
+
+echo "This script will:"
+echo "================="
+echo "Push commits and tags upstream"
+echo ""
+echo "Are you sure you want to continue? (y/n)"
+
+read PROCEED
+
+if [ "$PROCEED" != "y" ]; then
+    echo "Aborting"
+    exit 1
+fi
+
+echo "Proceeding..."
+
+set -e
+TEMP_WORKING_DIR=~/tmp/narayana/$CURRENT/sources/
+mkdir -p $TEMP_WORKING_DIR
+cd $TEMP_WORKING_DIR || fatal
+
+for REPO in documentation quickstart performance narayana
+do
+  echo ""
+  echo "=== GIT PUSHING $REPO ==="
+  echo ""
+
+  cd $REPO
+  git push origin $BRANCH --tags || fatal
+  cd ..
+done
