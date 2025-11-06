7f91d463c83b5ba2e6af74390733700d152c08d9
Marco Sappé Griot <msappegr@redhat.com>
2025-11-06 12:32:03 +0100

    Remove redundant setting, already in build.sh

--- narayana-release-process.sh
+++ narayana-release-process.sh
@@ -95,7 +95,7 @@ if [ ! $SNAPSHOT ]; then
   ORSON_PATH=$PWD/ext/
 
   # uploaded artifacts go straight live without the ability to close the repo at the end, so the install is done to verify that the build will work
-  ./build.sh clean install -Dmaven.repo.local=${PWD}/localm2repo -Dorson.jar.location=$ORSON_PATH -DskipTests -Pcommunity -Drelease -DreleaseStaging
+  ./build.sh clean install -Dmaven.repo.local=${PWD}/localm2repo -DskipTests -Pcommunity -Drelease -DreleaseStaging
   if [[ $? != 0 ]]; then
     echo 1>&2 Could not install narayana
     exit
@@ -108,7 +108,7 @@ fi
 
 # It is important in the deploy step that, if you are deploying, you provide a reference to your settings file as the ./build.sh overrides the default settings file discovery of Maven.
 # Please see https://github.com/jbosstm/narayana/wiki/Narayana-Release-Process for details of the settings.xml requirements
-./build.sh clean deploy -Dmaven.repo.local=${PWD}/localm2repo -DskipTests -gs ~/.m2/settings.xml -Dorson.jar.location=$ORSON_PATH -Pcommunity -Drelease -DreleaseStaging
+./build.sh clean deploy -Dmaven.repo.local=${PWD}/localm2repo -DskipTests -gs ~/.m2/settings.xml -Pcommunity -Drelease -DreleaseStaging
 if [[ $? != 0 ]]; then
   echo 1>&2 Could not deploy narayana
   exit
